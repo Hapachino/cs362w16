8aa0ef6fcb372577a3cd2620bf536f035d85b76e
riberajo <riberajo@oregonstate.edu>
2016-01-12 13:22:58 -0800

    refactored Mine card

--- projects/riberajo/dominion/dominion.c
+++ projects/riberajo/dominion/dominion.c
@@ -713,45 +713,14 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 
     case feast:
 
-    return playFeast(choice1, state);
+      return playFeast(choice1, state);
 
     case gardens:
       return -1;
 
     case mine:
-      j = state->hand[currentPlayer][choice1];  //store card we will trash
 
-      if (state->hand[currentPlayer][choice1] < copper || state->hand[currentPlayer][choice1] > gold)
-	{
-	  return -1;
-	}
-
-      if (choice2 > treasure_map || choice2 < curse)
-	{
-	  return -1;
-	}
-
-      if ( (getCost(state->hand[currentPlayer][choice1]) + 3) > getCost(choice2) )
-	{
-	  return -1;
-	}
-
-      gainCard(choice2, state, 2, currentPlayer);
-
-      //discard card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-
-      //discard trashed card
-      for (i = 0; i < state->handCount[currentPlayer]; i++)
-	{
-	  if (state->hand[currentPlayer][i] == j)
-	    {
-	      discardCard(i, currentPlayer, state, 0);
-	      break;
-	    }
-	}
-
-      return 0;
+      return playMine(handPos, choice1, choice2, state);
 
     case remodel:
       j = state->hand[currentPlayer][choice1];  //store card we will trash
@@ -1278,6 +1247,7 @@ int updateCoins(int player, struct gameState *state, int bonus)
 
   return 0;
 }
+
 int playFeast(int choice1, struct gameState *state) {
   //gain card with cost up to 5
   //Backup hand
@@ -1329,7 +1299,40 @@ int playFeast(int choice1, struct gameState *state) {
   return 0;
 }
 //Reset Hand
+int playMine(int handPos, int choice1, int choice2, struct gameState *state){
+    j = state->hand[currentPlayer][choice1];  //store card we will trash
 
+    if (state->hand[currentPlayer][choice1] < copper || state->hand[currentPlayer][choice1] > gold)
+    {
+      return -1;
+    }
+
+    if (choice2 > treasure_map || choice2 < curse)
+      {
+      return -1;
+      }
+
+    if ( (getCost(state->hand[currentPlayer][choice1]) + 3) > getCost(choice2) )
+      {
+      return -1;
+      }
+
+    gainCard(choice2, state, 2, currentPlayer);
+
+    //discard card from hand
+    discardCard(handPos, currentPlayer, state, 0);
+
+    //discard trashed card
+    for (i = 0; i < state->handCount[currentPlayer]; i++)
+    {
+      if (state->hand[currentPlayer][i] == j)
+      {
+        discardCard(i, currentPlayer, state, 0);
+        break;
+      }
+    }
+  return 0;
+}
 
 
 //end of dominion.c

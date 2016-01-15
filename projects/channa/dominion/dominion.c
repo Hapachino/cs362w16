d02c52db921bc0e1647cb09987f9b6aa9e72a36b
Nancy Chan <nancychannc@gmail.com>
2016-01-14 16:20:34 -0800

    Refactored sea_hag

--- projects/channa/dominion/dominion.c
+++ projects/channa/dominion/dominion.c
@@ -1154,14 +1154,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return 0;
 		
     case sea_hag:
-      for (i = 0; i < state->numPlayers; i++){
-	if (i != currentPlayer){
-	  state->discard[i][state->discardCount[i]] = state->deck[i][state->deckCount[i]--];			    state->deckCount[i]--;
-	  state->discardCount[i]++;
-	  state->deck[i][state->deckCount[i]--] = curse;//Top card now a curse
-	}
-      }
-      return 0;
+      return playSea_Hag(i, state, currentPlayer);
 		
     case treasure_map:
       //search hand for another treasure_map
@@ -1335,6 +1328,18 @@ int playSmithy(int i, int currentPlayer, struct gameState *state, int handPos)
       
       //discard card from hand
       discardCard(handPos, currentPlayer, state, 0);
+      return 0;
+}
+
+int playSea_Hag(int i, struct gameState *state, int currentPlayer)
+{
+  for (i = 0; i < state->numPlayers; i++){
+  if (i != currentPlayer){
+    state->discard[i][state->discardCount[i]] = state->deck[i][state->deckCount[i]--];          state->deckCount[i]--;
+    state->discardCount[i]++;
+    state->deck[i][state->deckCount[i]--] = curse;//Top card now a curse
+  }
+      }
       return 0;
 }
 

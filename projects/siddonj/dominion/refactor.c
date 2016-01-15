37311d4d0a01ea951845043200a3bd88aa02478a
justin <justin@delaris.com>
2016-01-15 03:43:28 -0800

    Broke 'adventurer' card into 'playAdventurer()' method.

---
+++ projects/siddonj/dominion/refactor.c
@@ -0,0 +1,11 @@
+Changes to Code
+
+smithy
+  Created 'playSmithy()' method.
+  Changed for loop from '<' to '<='.
+  User will draw one extra card.
+
+
+adventurer
+  Created 'playAdventurer()' method.
+  

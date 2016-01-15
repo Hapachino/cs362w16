2ef333d3ba358db2803e83a38e8de706ca34d649
EricAOlson <olsoeric@onid.oregonstate.edu>
2016-01-14 18:27:02 -0800

    Added documentation.c and refactored dominon.c & dominion.h to pull out 5 card types into standalone functions.

--- projects/olsoeric/dominion/dominion.h
+++ projects/olsoeric/dominion/dominion.h
@@ -130,11 +130,28 @@ int getWinners(int players[MAX_PLAYERS], struct gameState *state);
 /* Set array position of each player who won (remember ties!) to
    1, others to 0 */
 
+/*** Following 5 functions added by Eric Olson for CS396 Assignment 2: ***/
+void smithy_play(int currentPlayer, struct gameState *state, int handPos);
+/* Executes Smithy card actions (draw 3 cards from deck) */
+
+void adventurer_play(int currentPlayer, struct gameState *state);
+/* Executes Adventurer card action (draw until 2 treasures) */
+
+void council_room_play(int currentPlayer, struct gameState *state);
+/* Executes Council Room card action (draw 4, +1 Buy, opponents draw 1) */
+
+void village_play(int currentPlayer, struct gameState *state, int handPos);
+/* Executes Village card action (draw 1, +2 Action) */
+
+void cutpurse_play(int currentPlayer, struct gameState *state, int handPos);
+/* Executes Cutpurse card action (+2 coins, -1 copper from each other player's hand)
+
+/**I believe this is leftover from some other student's work.
 int playAdventurer(struct gameState *state);
 int playSmithy(struct gameState *state, int handPos);
 int playVillage(struct gameState *state, int handPos);
 int playFeast(struct gameState *state, int choice1);
-int playCouncil_Room(struct gameState *state, int handPos);
+int playCouncil_Room(struct gameState *state, int handPos);*/
 
 
 #endif

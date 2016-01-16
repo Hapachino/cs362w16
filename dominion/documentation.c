f00924da0e2c5844905281ff7336efca1c03b688
Kendra Swafford <swaffork@flip1.engr.oregonstate.edu>
2016-01-16 15:57:13 -0800

    Finish documentation; try to fix indents in dominion.c

--- dominion/documentation.c
+++ dominion/documentation.c
@@ -1,16 +1,11 @@
-smithy:
-    case smithy:
-      //+3 Cards
-      for (i = 0; i < 3; i++)
-	{
-	  drawCard(currentPlayer, state);
-	}
-			
-      //discard card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-      return 0;
+/* smithy: */
+This card places three cards from the player's deck into the player's hand. It
+also moves the smithy card into the player's discard pile.
 
-adventurer:
+/* adventurer: */
+This card draws cards from the player's deck until the player draws three
+treasure cards total. All cards drawn from the player's deck that were not
+treasure cards are moved into the player's discard pile.
     case adventurer:
       while(drawntreasure<2){
     if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
@@ -32,7 +27,16 @@ adventurer:
       }
       return 0;
 
-discardCard():
+/* discardCard(): */
+This function removes the card the player is currently holding from handPos and
+moves it either into the trash pile (if the trashFlag is 1), thereby removing
+the card from the game, OR into the player's discard pile. The side effects are
+incrementing the count of played cards (if card was discarded) and decrementing
+the count of cards still in the player's hand. Additionally, the card that the
+player is currently holding (in handPos) is updated to be the next card in the
+player's hand.
 
-updateCoins():
-
+/* updateCoins(): */
+This function effectively converts the copper, silver, and gold in the player's
+hand into coins. It also adds any bonuses to the player's coin count (which may
+come from cards like feast or cutpurse).

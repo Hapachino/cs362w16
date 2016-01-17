63a5b75eeb8c3490a1e0777167c540ce77cf40ea
Kendra Swafford <swaffork@flip3.engr.oregonstate.edu>
2016-01-16 19:23:09 -0800

    Complete documentation.c; complete playAdventurer(), now debug

--- dominion/documentation.c
+++ dominion/documentation.c
@@ -6,26 +6,6 @@ also moves the smithy card into the player's discard pile.
 This card draws cards from the player's deck until the player draws three
 treasure cards total. All cards drawn from the player's deck that were not
 treasure cards are moved into the player's discard pile.
-    case adventurer:
-      while(drawntreasure<2){
-    if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
-      shuffle(currentPlayer, state);
-    }
-    drawCard(currentPlayer, state);
-    cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
-    if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
-      drawntreasure++;
-    else{
-      temphand[z]=cardDrawn;
-      state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
-      z++;
-    }
-      }
-      while(z-1>=0){
-    state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
-    z=z-1;
-      }
-      return 0;
 
 /* discardCard(): */
 This function removes the card the player is currently holding from handPos and

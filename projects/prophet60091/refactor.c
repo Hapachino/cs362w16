04e3f3134336a6216e83e651db7cf81e7c81bbfe
prophet60091 <jackrobe@onid.oregonstate.edu>
2016-01-17 19:06:36 +0000

    Refactor done for Village, CouncilRoom, Baron, and Smithy

--- projects/prophet60091/refactor.c
+++ projects/prophet60091/refactor.c
@@ -3,7 +3,9 @@ Second while loop should be (z - 1 >= 0)
 */
 
 /* Smithy bug
-
+should be
+for (i = 0; i < 3; i++)
+{
 */
 
 /*Council Room Bug
@@ -12,5 +14,18 @@ second for loop i is not initilaized to 0
 //Each other player draws a card
 for ( i = 0; i < state->numPlayers; i++)
 {
+
+*/
+
+/*Village Bug 
+missing statement:
+//+1 Card
+drawCard(currentPlayer, state);
+
+*/
+
+/*baron Bug
+should be:
+int card_not_discarded = 1;
 
 */

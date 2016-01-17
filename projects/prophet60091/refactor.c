f9e155bbcc8ef5b82ac740c1121b02883de6e2ee
prophet60091 <jackrobe@onid.oregonstate.edu>
2016-01-17 21:24:06 +0000

    added description of changes to refactor.c

--- projects/prophet60091/refactor.c
+++ projects/prophet60091/refactor.c
@@ -1,3 +1,23 @@
+/*
+Made changes to dominion.c
+Include the addition of the following functions:
+int adventurerCard(struct gameState * state);
+int baronCard(struct gameState *state, int choice1);
+int smithyCard(struct gameState *state, int handPos);
+int councilRoomCard(struct gameState *state, int handPos);
+int villageCard(struct gameState *state, int handPos);
+
+
+They take the place of existing code that was present in the swtich statement of cardEffect.
+
+The same arguments that are present in the cardEffect function are also used again in the the 
+implemented card functions. With the exception of iterators, and the currentPlayer declaration
+ which as only set on a case by case basis within the function. 
+
+
+*/
+
+
 /* Adventurer bug  
 Second while loop should be (z - 1 >= 0)
 */

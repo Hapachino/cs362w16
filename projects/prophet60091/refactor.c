213700c7b12203c67d8ac3d8a2990d300655ca82
prophet60091 <jackrobe@onid.oregonstate.edu>
2016-01-17 18:29:59 +0000

    added refactor .c, refactored council room, added bugs

---
+++ projects/prophet60091/refactor.c
@@ -0,0 +1,16 @@
+/* Adventurer bug  
+Second while loop should be (z - 1 >= 0)
+*/
+
+/* Smithy bug
+
+*/
+
+/*Council Room Bug
+
+second for loop i is not initilaized to 0
+//Each other player draws a card
+for ( i = 0; i < state->numPlayers; i++)
+{
+
+*/

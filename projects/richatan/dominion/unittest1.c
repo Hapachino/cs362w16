91e418bf10c9193af8f8ea8e3de4976dcfc9fecc
tannarichardson <richatan@oregonstate.edu>
2016-01-20 13:24:25 -0800

    Added random gameState generator for unit tests

---
+++ projects/richatan/dominion/unittest1.c
@@ -0,0 +1,29 @@
+/*Tanna Richardson
+  CS362 - Winter 2016
+  Assignment 3
+  Unit test for: shuffle()
+*/
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include "test_helpers.h"
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main () {
+	int i, j;
+	struct gameState *g;
+		
+	//Generate random games and print current game state
+	for (i = 0; i < 5; i++){
+		g = randomGame();
+		
+		printf("Game #%d state:\n", i);
+		printGameState(g);
+		
+	}
+		
+	return 0;
+}

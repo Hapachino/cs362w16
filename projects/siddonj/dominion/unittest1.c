3ab5e866cacf95907aa98b3d878894ba06959344
justin <justin@delaris.com>
2016-01-22 11:52:08 -0500

    Modified Makefile to accomidate 'unittest1'. Unittest1 is just printing 'Hello' string right now.

---
+++ projects/siddonj/dominion/unittest1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "dominion.h"
+
+
+void testInitializeGame() {
+  printf("%s", "Hello");
+}
+
+
+int main() {
+  testInitializeGame();
+  return 0;
+}

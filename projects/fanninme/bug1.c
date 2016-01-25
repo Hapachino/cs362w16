367f15c02d94439d4a2f257aaf984c6865040ba7
Megan Fanning <megantfanning@gmail.com>
2016-01-24 20:53:45 -0800

    added bug1.c file

---
+++ projects/fanninme/bug1.c
@@ -0,0 +1,41 @@
+//unit tests
+
+1)Playcard unittest1: Play card function
+Testing playCard.
+RANDOM TESTS.
+Error in playCard.
+unitTest1: unitTest1.c:44: unitTest: Assertion `success == 0' failed.
+Aborted (core dumped)
+
+2) unittest2: game is over function
+segfault assert post→supply-count[providence] > 0 failed
+
+3)Testing end turn function.
+RANDOM TESTS.
+Segmentation fault (core dumped)
+
+4)Unit Test 4:
+RANDOM TESTS.
+unitTest4: unitTest4.c:34: unitTest: Assertion `success > 500' failed.
+Aborted (core dumped)
+this implies that the count was greater than the max deck count
+
+5) card unit test 1: Adventurer
+Testing adventurer card.
+RANDOM TESTS.
+Segmentation fault (core dumped)
+
+6)card unit test 2:
+Testing Feast Card.
+RANDOM TESTS.
+Segmentation fault (core dumped)
+
+7)card unit test 3:
+Testing smithy Card.
+RANDOM TESTS.
+Segmentation fault (core dumped)
+
+8)card unit test 4:
+Testing village Card.
+RANDOM TESTS.
+Segmentation fault (core dumped)
--- projects/fanninme/dominion/unitTest1.c
+++ projects/fanninme/dominion/unitTest1.c
@@ -12,6 +12,7 @@
 #define DEBUG 0
 #define NOISY_TEST 1
 
+//code leveraged betterCard function heavily.
 //Unit test for playCard function
 //Preconditions:
 //function accepts int handPos, int choice1, int choice2, int choice3, struct gameState *state) 
@@ -23,7 +24,7 @@ int unitTest(struct gameState *post){
     srand(time(NULL));
 
     //define variables
-    int sucess;
+    int success;
     struct gameState pre;
     memcpy(&pre,post,sizeof(struct gameState));
     //create an array to hold four integer inputs
@@ -34,10 +35,13 @@ int unitTest(struct gameState *post){
     }
 
     //call function
-    sucess=playCard(input[0],input[1],input[2],input[3], post);
+    success=playCard(input[0],input[1],input[2],input[3], post);
     //memcmp game state size
     assert (memcmp(&pre,post, sizeof(struct gameState))==0);
-    assert (sucess == 0);
+    if (success == -1){
+        printf ("Error in playCard.\n");
+    }
+    assert (success == 0);
     //specific function check 
     //confirm that actions is one less
     assert (pre.numActions == post->numActions+1);
@@ -45,53 +49,59 @@ int unitTest(struct gameState *post){
     return 0;
 }
 
+
 int main () {
   //define variables  
-  int i, n, r, player, deckCount, discardCount, handCount;
+  int i, n, r, p, deckCount, discardCount, handCount;
   //define a array of cards
   int k[10] = {adventurer, council_room, feast, gardens, mine,
 	       remodel, smithy, village, baron, great_hall};
   //define a gamestate
   struct gameState G;
 
-  printf ("RANDOM TESTS.\n");
+  printf ("Testing playCard.\n");
 
+  printf ("RANDOM TESTS.\n");
+  //create random seed
+  SelectStream(2);
+  PutSeed(3);
   //for 2000 test cases
   for (n = 0; n < 2000; n++) {
     for (i = 0; i < sizeof(struct gameState); i++) {
-      ((char*)&G)[i] = floor(rand() * 256);
+      //fill gamestate with random bits between 0-256 using ofset
+      ((char*)&G)[i] = floor(Random() * 256);
     }
-    player = floor(rand() * 2);
-    //game state
-    G.deckCount[player] = floor(rand() * MAX_DECK);
-    G.discardCount[player] = floor(rand() * MAX_DECK);
-    G.handCount[player] = floor(rand() * MAX_HAND);
-    //call unit test function with test input
+    p = floor(Random() * 2);
+    G.deckCount[p] = floor(Random() * MAX_DECK);
+    G.discardCount[p] = floor(Random() * MAX_DECK);
+    G.handCount[p] = floor(Random() * MAX_HAND);
+    //call function with test input
     unitTest(&G);
+
   }
   printf ("ALL TESTS OK\n");
-  exit(0);
 
   //fixed tests
   printf ("SIMPLE FIXED TESTS.\n");
-  for (player = 0; player < 2; player++) {
+  for (p = 0; p < 2; p++) {
     for (deckCount = 0; deckCount < 5; deckCount++) {
       for (discardCount = 0; discardCount < 5; discardCount++) {
-	for (handCount = 0; handCount < 5; handCount++) {
-	  memset(&G, 23, sizeof(struct gameState)); 
-	  r = initializeGame(2, k, 1, &G);  
-	  G.deckCount[player] = deckCount;
-	  memset(G.deck[player], 0, sizeof(int) * deckCount);
-	  G.discardCount[player] = discardCount;
-	  memset(G.discard[player], 0, sizeof(int) * discardCount);
-	  G.handCount[player] = handCount;
-	  memset(G.hand[player], 0, sizeof(int) * handCount);
-      //call unit test
-	  unitTest(&G);
-	}
-      }
+	    for (handCount = 0; handCount < 5; handCount++) {
+	        memset(&G, 23, sizeof(struct gameState)); 
+	        r = initializeGame(2, k, 1, &G);
+	        G.deckCount[p] = deckCount;
+	        memset(G.deck[p], 0, sizeof(int) * deckCount);
+	        G.discardCount[p] = discardCount;
+	        memset(G.discard[p], 0, sizeof(int) * discardCount);
+	        G.handCount[p] = handCount;
+	        memset(G.hand[p], 0, sizeof(int) * handCount);
+	        //run unit test.
+            unitTest(&G);
+	        }
+        }
     }
   }
+  printf ("ALL TESTS OK\n");
 
   return 0;
 }

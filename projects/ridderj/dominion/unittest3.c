80671692c94b6eac57a6a1feecd3d5dfeb7056a5
Jason Ridder <jason@oregonstate.edu>
2016-01-27 12:03:06 -0800

    Added unitest4.c for func isGameOver()

--- projects/ridderj/dominion/unittest3.c
+++ projects/ridderj/dominion/unittest3.c
@@ -48,7 +48,7 @@ int main() {
     
     
     
-    printf ("\nTESTING gainCard():\n");
+    printf ("\n\nTESTING gainCard():\n");
     
     
     printf("\nTest 1 - gainCard should return -1 if supplyCount is empty\n");
@@ -63,7 +63,7 @@ int main() {
     }
     
     returnValue = gainCard(duchy, &G, 0, 0);
-    printf("return value = %d, expected = -1\n\n", returnValue);
+    printf("return value = %d, expected = -1\n", returnValue);
     assert(returnValue == -1);
     
     
@@ -129,56 +129,24 @@ int main() {
     assert(G.handCount[0] == 1); // check hand pile for correct count of cards
     assert(G.hand[0][0] == 0); // check hand pile for card
     
+   
+   
+   
     
-/*     //Test both players
-    for (p = 0; p < numPlayer; p++){
-       //Used to cycle thru the 6 cards that actually have point values
-       for (testAllCards = 0; testAllCards < 6; testAllCards++){
-          //Next 3 for() statements used to check various sizes of decks
-          //from 0 cards in all decks to 5 cards in each deck.
-         for (deckCards = 0; deckCards <= totalCards; deckCards++){
-          
-          for (handCards = 0; handCards <= totalCards; handCards++){
-             
-             for (discardCards = 0; discardCards <= totalCards; discardCards++){
-                  
-                  
-                
-                      printf("\nTesting player %d with %d %s card(s).\nAmount in location: Deckcards: %d  HandCards: %d DiscardCards: %d. Point Value: %d\n", p, deckCards + handCards + discardCards, cardNames[pointCards[testAllCards]], deckCards, handCards, discardCards,pointValue[testAllCards]);
-                      memset(&G, 23, sizeof(struct gameState));   // clear the game state
-                      r = initializeGame(numPlayer, k, seed, &G); // initialize a new game
-                      
-                      
-                      
-                      G.deckCount[p] = deckCards;                 // set the number of cards deckCard
-                      memcpy(G.deck[p], nameCards[pointCards[testAllCards]], (sizeof(int) * deckCards)); // set all the cards to current test card "nameCards"      
-                      G.handCount[p] = handCards;                 // set the number of cards on handCount
-                      memcpy(G.hand[p], nameCards[pointCards[testAllCards]], (sizeof(int) * handCards)); // set all the cards to current test card "nameCards"
-                      G.discardCount[p] =discardCards;                 // set the number of cards on discardCount
-                      memcpy(G.discard[p], nameCards[pointCards[testAllCards]], (sizeof(int) * discardCards)); // set all the cards to current test card "nameCards"
-                      
-                      
-                      
-                      score = scoreFor(p,&G);
-                      //Gardens card has a different oracle test so it's included in the else when tested. 
-                      if(testAllCards != 5) {
-                        printf("Score = %d, expected = %d ", score,(deckCards + handCards + discardCards)*(pointValue[testAllCards]));
-                        if(score != ((deckCards + handCards + discardCards)*(pointValue[testAllCards]))){
-                           printf("TEST FAILED\n");
-                        }
-                      } else {
-                         printf("Score = %d, expected = %d ", score,(deckCards + handCards + discardCards)/10);
-                         if(score != ((deckCards + handCards + discardCards)/(10))){
-                           printf("TEST FAILED\n");
-                        }
-                      }
-                      
-                 
-             } //END discardCards
-          } //END handCards
-         } //END deckCards
-       } //END testAllCards
-    } // END player */
+    printf("\nTest 5 - Testing supplyCount decressed in size after func completed.\n");
+    
+    memset(&G, 23, sizeof(struct gameState));   // clear the game state
+    r = initializeGame(numPlayer, k, seed, &G); // initialize a new game
+    
+    G.supplyCount[curse] = 1;
+    G.handCount[0] = 0;
+    G.hand[0][0] = 10; //put gardens card in initially
+    
+    returnValue = gainCard(curse, &G, 2, 0);
+    
+    printf("supply count for curse = %d, expected supply count for curse = 0\n\n",G.supplyCount[curse]);
+    
+    assert(G.supplyCount[curse] == 0); // check hand pile for correct count of cards
     
     
     

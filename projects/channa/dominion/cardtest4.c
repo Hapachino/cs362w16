73ea39684861dc8d040bd8e17502b1233efd14a5
Nancy Chan <nancychannc@gmail.com>
2016-01-27 14:06:55 -0800

    Initial commit of card test 4

---
+++ projects/channa/dominion/cardtest4.c
@@ -0,0 +1,43 @@
+/* -----------------------------------------------------------------------
+ * test remodel
+ * 
+ *
+ * cardtest4: cardtest4.c dominion.o rngs.o
+ *      gcc -o cardtest4 -g  cardtest4.c dominion.o rngs.o $(CFLAGS)
+ * -----------------------------------------------------------------------
+ */
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdbool.h>
+#include "rngs.h"
+
+int main() {
+    int i;
+    int seed = 1000;
+
+    int numPlayer = 3;
+    int p, r;
+    int k[10] = {adventurer, great_hall, cutpurse, gardens, mine
+               , remodel, smithy, village, sea_hag, embargo};
+
+    struct gameState G;
+    bool pass = true;
+
+    memset(&G, 23, sizeof(struct gameState));   // clear the game state
+    r = initializeGame(numPlayer, k, seed, &G); // initialize a new game
+
+    
+
+    if (pass) {
+        printf("\nAll tests passed!");
+    }
+    else {
+        printf("\nSome test(s) failed!");
+    }
+    
+    return 0;
+}

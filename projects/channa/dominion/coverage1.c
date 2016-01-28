7c9a940ee7d7dfeea28b178e5efd096615b78ba5
Nancy Chan <nancychannc@gmail.com>
2016-01-28 15:51:13 -0800

    Added cumulative coverage for all 8 tests

--- projects/channa/dominion/coverage1.c
+++ projects/channa/dominion/coverage1.c
@@ -3,8 +3,13 @@ CS 362
 Winter 2016
 Assignment 3: coverage1.c
 
+Aggregate coverage for all 8 test files:
+Lines executed:37.63% of 558
+Branches executed:47.96% of 417
+Taken at least once:36.21% of 417
+Calls executed:21.05% of 95
+
 unittest1:
-	
 
 unittest2:
 

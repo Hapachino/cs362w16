339bd7b91b4238ab8a218cd2905b96fd345babf3
mendojen <mendojen@onid.oregonstate.edu>
2016-01-28 10:52:29 -0800

    unittest1.c updates

--- projects/mendojen/dominion/unittest1.c
+++ projects/mendojen/dominion/unittest1.c
@@ -1,7 +1,82 @@
- smithy- Draw 3+ cards. When this card is played, the drawCard() and discardCard() functions are called. The card count in a player’s hand is increased by 3 (by calling drawCard() function 3 times) while the deck card count is decreased. The smithy card is then discarded with the discardCard() function.
- 
- adventurer- Reveal cards from your deck until you reveal 2 Treasure cards. Put those Treasure cards into your hand and discard the other revealed cards. In the code, the number of treasures are being tracked by a variable. DrawCard() function is again called to draw until drawntreasure is 2. If the deck runs out of cards, the discard deck is shuffled. The cards that are not treasures are discarded.
- 
- discardCard()- First, the trashFlag integer variable is checked to see if it is a 0 or 1. If it is 0, the card is added to the played pile (or discard pile). If trashFlag is 1, this means that the card was trashed and is removed from the game. This function also keeps track of the number of cards that are still in a player’s hand. 
- 
- updateCoins()- This function updates the number of coins a player has in their hand depending on the type of card. First, the coin count is set to 0. Then, each card is read to see if the card is a copper, silver, or gold. If copper, 1 is added to the coin count. If the card is silver, 2 is added, and if card is gold, 3 is added. At the end, a bonus is added. Once the function completes, the total number of coins in a player’s hand is counted.
+/* -----------------------------------------------------------------------
+ * Demonstration of how to write unit tests for dominion-base
+ * Include the following lines in your makefile:
+ *
+ * testUpdateCoins: testUpdateCoins.c dominion.o rngs.o
+ *      gcc -o testUpdateCoins -g  testUpdateCoins.c dominion.o rngs.o $(CFLAGS)
+ * -----------------------------------------------------------------------
+ */
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+
+// set NOISY_TEST to 0 to remove printfs from output
+#define NOISY_TEST 1
+
+int main() {
+    int i;
+    int seed = 1000;
+    int numPlayer = 2;
+    int maxBonus = 10;
+    int p, r, handCount;
+    int bonus;
+    int k[10] = {adventurer, council_room, feast, gardens, mine
+               , remodel, smithy, village, baron, great_hall};
+    struct gameState G;
+    int maxHandCount = 5;
+    // arrays of all coppers, silvers, and golds
+    int coppers[MAX_HAND];
+    int silvers[MAX_HAND];
+    int golds[MAX_HAND];
+    for (i = 0; i < MAX_HAND; i++)
+    {
+        coppers[i] = copper;
+        silvers[i] = silver;
+        golds[i] = gold;
+    }
+
+    printf ("TESTING updateCoins():\n");
+    for (p = 0; p < numPlayer; p++)
+    {
+        for (handCount = 1; handCount <= maxHandCount; handCount++)
+        {
+            for (bonus = 0; bonus <= maxBonus; bonus++)
+            {
+#if (NOISY_TEST == 1)
+                printf("Test player %d with %d treasure card(s) and %d bonus.\n", p, handCount, bonus);
+#endif
+                memset(&G, 23, sizeof(struct gameState));   // clear the game state
+                r = initializeGame(numPlayer, k, seed, &G); // initialize a new game
+                G.handCount[p] = handCount;                 // set the number of cards on hand
+                memcpy(G.hand[p], coppers, sizeof(int) * handCount); // set all the cards to copper
+                updateCoins(p, &G, bonus);
+#if (NOISY_TEST == 1)
+                printf("G.coins = %d, expected = %d\n", G.coins, handCount * 1 + bonus);
+#endif
+                assert(G.coins == handCount * 1 + bonus); // check if the number of coins is correct
+
+                memcpy(G.hand[p], silvers, sizeof(int) * handCount); // set all the cards to silver
+                updateCoins(p, &G, bonus);
+#if (NOISY_TEST == 1)
+                printf("G.coins = %d, expected = %d\n", G.coins, handCount * 2 + bonus);
+#endif
+                assert(G.coins == handCount * 2 + bonus); // check if the number of coins is correct
+
+                memcpy(G.hand[p], golds, sizeof(int) * handCount); // set all the cards to gold
+                updateCoins(p, &G, bonus);
+#if (NOISY_TEST == 1)
+                printf("G.coins = %d, expected = %d\n", G.coins, handCount * 3 + bonus);
+#endif
+                assert(G.coins == handCount * 3 + bonus); // check if the number of coins is correct
+            }
+        }
+    }
+
+    printf("All tests passed!\n");
+
+    return 0;
+}

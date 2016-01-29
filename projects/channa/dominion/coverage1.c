7b4a832aea342ba4ce8e8fd99119930786566724
Nancy Chan <nancychannc@gmail.com>
2016-01-28 16:24:57 -0800

    Updated coverage from FLIP

--- projects/channa/dominion/coverage1.c
+++ projects/channa/dominion/coverage1.c
@@ -4,7 +4,7 @@ Winter 2016
 Assignment 3: coverage1.c
 
 Aggregate coverage for all 8 test files:
-Lines executed:37.63% of 558
+Lines executed:36.70% of 575
 Branches executed:47.96% of 417
 Taken at least once:36.21% of 417
 Calls executed:21.05% of 95

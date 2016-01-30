94eb8ad6c709c427d723ad5f49f27be06604a887
Jon <jonlagrew@gmail.com>
2016-01-30 13:57:57 -0800

    updated coverage

--- projects/lagrewj/dominion/coverage1.c
+++ projects/lagrewj/dominion/coverage1.c
@@ -1,18 +1,16 @@
+/*
 Jonathan Lagrew
 CS 362
 Winter 2016
 Assignment 3: coverage1.c
 
-Aggregate coverage for all 8 test files:
-Lines executed:36.70% of 575
-Branches executed:47.96% of 417
-Taken at least once:36.21% of 417
-Calls executed:21.05% of 95
+Summary of coverage for all tests:
+File 'dominion.c'
+Lines executed:35.51% of 583
+Branches executed:37.89% of 417
+Taken at least once:33.09% of 417
+Calls executed:22.00% of 100
 
-unittest1:
 
-unittest2:
-
-unittest3:
-
-unittest4:
+	
+*/

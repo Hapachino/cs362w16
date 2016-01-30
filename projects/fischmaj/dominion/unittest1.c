4b5c218621a99dc7b3f65a1b2bab8aa3dab9eff7
fischmaj <fischmaj@onid.oregonstate.edu>
2016-01-30 05:54:11 -0600

    got card count working in unittest1.c

--- projects/fischmaj/dominion/unittest1.c
+++ projects/fischmaj/dominion/unittest1.c
@@ -36,18 +36,21 @@
 
 
 /* function prototypes */
+
 int compareGameState(struct gameState *old, struct gameState *new, 
 		     int *diffArray, int length);
 
-int countCards(struct gameState *state, int *deck, int decklength); 
+int countCards(int *deck, int *cardCount);
+
 int checkShuffle(int player, struct gameState *pre); 
+
+
 
 
 
 int main(){
 
-  int n, i, p; 
-  int diffArray[MEMBERS];  /* array to record changed members of gamestate*/
+  int n, i, j, p; 
  
   struct gameState pre;
 
@@ -70,6 +73,14 @@ int main(){
     pre.deckCount[p]= floor(Random() * MAX_DECK);
     pre.discardCount[p] = floor(Random() * MAX_DECK);
     pre.handCount[p] = floor(Random() * MAX_HAND); 
+
+    /* Fill hands with random cards from this kindom */
+    for (i = 0; i < MAX_PLAYERS; i++){
+      for( j=0; j < MAX_DECK; j++){
+	pre.hand[i][j]=floor(Random()*CARDTYPES);
+      }
+    }
+    printf("\nTest run %i:", n);
     checkShuffle(p, &pre);
   
  }
@@ -90,14 +101,14 @@ int compareGameState(struct gameState *old, struct gameState *new,
 
   int i, j, k;
   
-  struct gameState difference; 
+  struct gameState *difference= malloc(sizeof(struct gameState)); 
 
  
   /* generating the difference between gameState old and gameState new using */
   /* bitwise XOR */
   char *charPtr1 =(char *)old; 
   char *charPtr2 =(char *) new; 
-  char *charPtr3 =(char *) &difference;  
+  char *charPtr3 =(char *) difference;  
   for (i = 0; i < sizeof(struct gameState); i++){
 
     /*generating a struct gamestate called differences that is the XOR of */
@@ -108,21 +119,21 @@ int compareGameState(struct gameState *old, struct gameState *new,
 
 
   /* checking which members of new were changed, member by member */
-  if (difference.numPlayers)    {diffArray[0]=1;}
+  if (difference->numPlayers)    {diffArray[0]=1;}
 
   /* need to check every type of card for cardSupply and embargoTokens*/
   for (i = 0; i < CARDTYPES; i++){
-    if (difference.supplyCount[i])   {diffArray[1] = 1;}
-    if (difference.embargoTokens[i]) {diffArray[2] = 1;}
+    if (difference->supplyCount[i])   {diffArray[1] = 1;}
+    if (difference->embargoTokens[i]) {diffArray[2] = 1;}
   }
 
-  if (difference.outpostPlayed)  {diffArray[3]=1; }
-  if (difference.outpostTurn)    {diffArray[4]=1; }
-  if (difference.whoseTurn)      {diffArray[5]=1;}
-  if (difference.phase)          {diffArray[6]=1;  }
-  if (difference.numActions)     {diffArray[7]=1;  }
-  if (difference.coins)          {diffArray[8]=1;} 
-  if (difference.numBuys)        {diffArray[9]=1;}
+  if (difference->outpostPlayed)  {diffArray[3]=1; }
+  if (difference->outpostTurn)    {diffArray[4]=1; }
+  if (difference->whoseTurn)      {diffArray[5]=1;}
+  if (difference->phase)          {diffArray[6]=1;  }
+  if (difference->numActions)     {diffArray[7]=1;  }
+  if (difference->coins)          {diffArray[8]=1;} 
+  if (difference->numBuys)        {diffArray[9]=1;}
  
   /*Checking all of the game states that depend on a particular player */
   for (j = 0; j < MAX_PLAYERS; j++){
@@ -130,12 +141,12 @@ int compareGameState(struct gameState *old, struct gameState *new,
     /*checking the player's hands */
     for (k = 0; k < MAX_HAND; k++){
       /*If a player's hand has changed, record a 1 in that digit */	
-      if (difference.hand[j][k])   {diffArray[10] += (int) pow(10, j);}
+      if (difference->hand[j][k])   {diffArray[10] += (int) pow(10, j);}
     }
 
 
     /*If a player's hand Count has changed, record a 1 in that digit */
-    if(difference.handCount[j])    {diffArray[11] += (int) pow(10, j);}
+    if(difference->handCount[j])    {diffArray[11] += (int) pow(10, j);}
 
 
     /*checking all of the decks */
@@ -144,7 +155,7 @@ int compareGameState(struct gameState *old, struct gameState *new,
       /* If any card in player j's deck has changed record player number */
       /* player 1 = 1, player 2=10, player 3= 100, etc.- then break out  */
       /* to examine the next player's deck.  */
-      if(difference.deck[j][k])    {
+      if(difference->deck[j][k])    {
 	diffArray[12] += (int) pow(10, j);
 	break;
       }
@@ -157,7 +168,7 @@ int compareGameState(struct gameState *old, struct gameState *new,
       /* If any card in player j's discard changed record player number  */
       /* player 1 = 1, player 2=10, player 3= 100, etc.- then break out  */
       /* to examine the next player's deck.  */
-      if(difference.discard[j][k]) {
+      if(difference->discard[j][k]) {
 	diffArray[14] += (int) pow(10, j);
 	break;
       }
@@ -165,16 +176,17 @@ int compareGameState(struct gameState *old, struct gameState *new,
     }
 
   
-    if(difference.deckCount[j])    {diffArray[13] += (int)pow(10, j); }
-    if(difference.discardCount[j]) {diffArray[15] += (int)pow(10, j); }
+    if(difference->deckCount[j])    {diffArray[13] += (int)pow(10, j); }
+    if(difference->discardCount[j]) {diffArray[15] += (int)pow(10, j); }
   }
 
   for (i = 0; i < MAX_DECK; i++){
-    if (difference.playedCards[i]) {diffArray[16] = 1;}
+    if (difference->playedCards[i]) {diffArray[16] = 1;}
   }
 
-  if (difference.playedCardCount)  {diffArray[17] = 1;}
+  if (difference->playedCardCount)  {diffArray[17] = 1;}
 
+  free(difference); 
   return 0; 
 }
 
@@ -183,26 +195,40 @@ int compareGameState(struct gameState *old, struct gameState *new,
 
 /* This function counts the cards in a card deck and records number of each */
 /* type of card in an array */
-int countCards(struct gameState *state, int *deck, int decklength){
+int countCards(int *deck, int *cardCount){
 
+  int i; 
+ 
+  for (i= 0; i < MAX_DECK; i++){
+    cardCount[deck[i]]++;
+  }
   return 0;
 } 
 
+
 int checkShuffle(int player, struct gameState *pre){
 
+
+  /* create two arrays to track the number of each cardtype in a deck */
+  int *cardListPre = malloc(sizeof(int)*CARDTYPES);
+  memset(cardListPre, 0, (sizeof(int)*CARDTYPES));
+ int *cardListPost = malloc(sizeof(int)*CARDTYPES);
+  memset(cardListPost, 0, (sizeof(int)*CARDTYPES));
+
+  /* create a copy of the input gameState */
   struct gameState *post = malloc(sizeof(struct gameState));
   memcpy(post, pre, sizeof(struct gameState)); 
-
-  /* Business Rule #1: The function accepts 1 player and a game state... */
-  shuffle(player, post); 
 
   /* create an array to track differences from pre to post and zeroize */
   int *differences = malloc(sizeof(int)*MEMBERS);
   memset(differences, 0, (sizeof(int)*MEMBERS));
 
-  /* **  tests still to accomplish: 
- ** 1. ... and the discard deck of the chosen player is permuted. 
-  
+
+
+  /* Business Rule #1: The function accepts 1 player and a game state... */
+  shuffle(player, post); 
+
+  /*  
  ** 3. Should result in the same number of each type of card in the player's
  **    deck following shuffling as existed in the deck before shuffling
  ** 
@@ -222,6 +248,21 @@ int checkShuffle(int player, struct gameState *pre){
   }
 
 
+  /*3. Should result in the same number of each type of card in the deck */
+  /*   following shuffling as existed before shuffling.*/
+  countCards(pre->hand[player], cardListPre);
+  countCards(post->hand[player], cardListPost);
+
+  for (int i = 0; i < CARDTYPES; i++){
+    printf("\n%d =?= %d\n", cardListPre[i], cardListPost[i]); 
+    if (cardListPre[i] != cardListPost[i]){
+      printf("\nshuffle() fails business rule #3: card types changed\n");
+      break;
+    }
+  }
+   
+
+
   /*5. The other features of the state of the game should all be unchanged. */
   for (int i =0; i < MEMBERS; i++){
     if ((differences[i]) && (i != 12)){
@@ -229,6 +270,9 @@ int checkShuffle(int player, struct gameState *pre){
       printf(" state param #%d fails\n with code %d", i, differences[i]); 
     }
   }
+
+
+
 
   free(post); 
   free(differences);

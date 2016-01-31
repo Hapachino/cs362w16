0ccdc0ae9252624efa38137b958cf5b542320756
Mark Giles <gilesm@flip1.engr.oregonstate.edu>
2016-01-31 13:58:58 -0800

    Updated bug1.c with card selection

---
+++ projects/gilesm/dominion/bug1.c
@@ -0,0 +1,107 @@
+--------------------------- NON CARD - UNIT TEST 1 ---------------------------
+Name: buyCard
+Parameters:
+	- int supplyPos
+	- struct gameState *state
+Return Type: int
+
+Description:
+
+Test Findings:
+
+
+--------------------------- NON CARD - UNIT TEST 2 ---------------------------
+Name: numHandCards
+Parameters:
+	- struct gameState *state
+Return Type: int
+
+Description:
+
+Test Findings:
+
+--------------------------- NON CARD - UNIT TEST 3 ---------------------------
+Name: gainCard
+Parameters:
+	- int supplyPos
+	- struct gameState *state
+	- int toFlag
+	- int player
+Return Type: int
+
+Description:
+
+Test Findings:
+
+
+--------------------------- NON CARD - UNIT TEST 4 ---------------------------
+Name: updateCoints
+Parameters:
+	- int player
+	- struct gameState *state
+	- int bonus
+Return Type: int
+
+Description:
+
+Test Findings:
+
+
+----------------------------- CARD - UNIT TEST 1 -----------------------------
+Name: smithy
+Parameters:
+	- int currentPlayer
+	- struct gameState *state
+	- int handpos
+Return Type: int
+
+Description:
+
+Test Findings:
+
+
+----------------------------- CARD - UNIT TEST 2 -----------------------------
+Name: adventurer
+Parameters:
+	- int currentPlayer
+	- int drawntreasure
+	- int cardDrawn
+	- int temphand[]
+	- struct gameState *state
+Return Type: int
+
+Description:
+
+Test Findings:
+
+
+----------------------------- CARD - UNIT TEST 3 -----------------------------
+Name: village
+Parameters:
+	- int currentPlayer
+	- int choice1
+	- int choice2
+	- struct gameState *state
+	- int handpos
+Return Type: int
+
+Description:
+
+Test Findings:
+
+
+----------------------------- CARD - UNIT TEST 4 -----------------------------
+Name: remodel
+Parameters:
+	- int currentPlayer
+	- int choice1
+	- int choice2
+	- struct gameState *state
+	- int handpos
+Return Type: int
+
+Description:
+
+Test Findings:
+
+

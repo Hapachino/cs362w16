8282ceeef78412d8d6bbaa0019611b6a2d63488f
justin <justin@delaris.com>
2016-01-31 09:26:10 -0500

    Finished cardtest3.

--- projects/siddonj/dominion/cardtest3.c
+++ projects/siddonj/dominion/cardtest3.c
@@ -11,13 +11,15 @@
 
 void testPlayRemodel() {
   int i = 0;
-  int playerHandSize = 0;       // Get hand size.
-  int playerActions = 0;
-  int handSizeDecrease = 0;
-  int actionDecrease = 0;
+
   int remodelHandCount = 0;
   int remodelDiscardCount = 0;
   int remodelPlayedCount = 0;
+
+  int adventurerHandCount = 0;
+  int adventurerDiscardCount = 0;
+  int adventurerPlayedCount = 0;
+
   int treasureCount = 0;        // Starting treasure amount.
   int increasedTreasure = 0;    // Amount treasure increased.
 
@@ -30,51 +32,51 @@ void testPlayRemodel() {
   int kingdomCards[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, remodel, tribute, smithy};
   int randomSeed = -1;                          // Set randomSeed to less than 0 so it is based off system clock in the initializeGame method.
   struct gameState *state = newGame();          // Initialize game state.
+  struct gameState *testGame = newGame();
 
+
+  initializeGame(numPlayers, kingdomCards, randomSeed, state);      // Initialize game with valid game values.
 
   printf("\n***** TESTING PLAY REMODEL *****\n");
-  // Initialize game with valid game values.
-  initializeGame(numPlayers, kingdomCards, randomSeed, state);
-  playerHandSize = numHandCards(state);       // Get hand size.
-  playerActions = state->numActions;          // Get initial action amount.
 
-  handSizeDecrease = 0;
+  memcpy(testGame, state, sizeof(struct gameState));                    // Setup clean test game.
+
   otherHandIncrease = 0;
   otherDeckIncrease = 0;
   otherDiscardIncrease = 0;
 
-
   // Set player 1's first card in hand to be adventurer card.
-  state->hand[state->whoseTurn][0] = remodel;
-  state->hand[state->whoseTurn][1] = adventurer;
+  testGame->hand[testGame->whoseTurn][0] = remodel;
+  testGame->hand[testGame->whoseTurn][1] = adventurer;
   int gardensDiscardCount = 0;
-  // (state, player, remodel card hand position, card position to buy, card hand position to trash)
-  playRemodel(state, state->whoseTurn, 0, 1, 1);      // Trying to buy gardens by trashing a adventurer.
 
-  handSizeDecrease = playerHandSize - numHandCards(state);       // Find amount players hand size increased by.
-  actionDecrease = playerActions - state->numActions;            // Find amount players action decreased by.
+  playRemodel(testGame, testGame->whoseTurn, 0, 1, 1);      // Trying to buy gardens by trashing a adventurer.
 
-  // See if any other players handsize increased, don't include player that played adventurer.
+  // See if any other players handsize increased, don't include player that played remodel.
   for(i = 1; i < numPlayers; i++) {
-    if(state->handCount[i] > 0) {
+    if(testGame->handCount[i] > 0) {
       otherHandIncrease++;
     }
-    if(state->deckCount[i] > 10) {
+    if(testGame->deckCount[i] > 10) {
       otherDeckIncrease++;
     }
-    if(state->discardCount[i] > 0) {
+    if(testGame->discardCount[i] > 0) {
       otherDiscardIncrease++;
     }
   }
 
-  // Check what player has in hand.
-  for(i = 0; i < numHandCards(state); i++) {
-    if(state->hand[state->whoseTurn][i] == remodel) {        // There is a remodel in hand.
+  // Check what player has in hand after playing remodel.
+  for(i = 0; i < numHandCards(testGame); i++) {
+    if(testGame->hand[testGame->whoseTurn][i] == remodel) {        // There is a remodel in hand.
       remodelHandCount++;
     }
 
+    if(testGame->hand[testGame->whoseTurn][i] == adventurer) {
+      adventurerHandCount++;
+    }
+
     // Count up # of treasures.
-    if(state->hand[state->whoseTurn][i] == copper || state->hand[state->whoseTurn][i] == silver || state->hand[state->whoseTurn][i] == gold ) {
+    if(testGame->hand[testGame->whoseTurn][i] == copper || testGame->hand[testGame->whoseTurn][i] == silver || testGame->hand[testGame->whoseTurn][i] == gold ) {
       increasedTreasure++;
     }
   }
@@ -82,93 +84,109 @@ void testPlayRemodel() {
   increasedTreasure = increasedTreasure - treasureCount;    // Determine total quantity treasure increased by.
 
   // Make sure player has no remodel cards in discard pile.
-  for(i = 0; i < state->discardCount[state->whoseTurn]; i++) {
-    if(state->discard[state->whoseTurn][i] == remodel) {        // There is a remodel in discard.
+  for(i = 0; i < testGame->discardCount[testGame->whoseTurn]; i++) {
+    if(testGame->discard[testGame->whoseTurn][i] == remodel) {        // There is a remodel in discard.
       remodelDiscardCount++;
     }
 
-    if(state->discard[state->whoseTurn][i] == gardens) {        // There is a gardens in discarded cards because we bought one.
+    if(testGame->discard[testGame->whoseTurn][i] == adventurer) {
+      adventurerDiscardCount++;
+    }
+
+    if(testGame->discard[testGame->whoseTurn][i] == gardens) {        // See how many gardens are in discarded cards because we bought one.
       gardensDiscardCount++;
     }
   }
 
   // Make sure player has 1 remodel card in played card pile.
-  for(i = 0; i < state->playedCardCount; i++) {
-    if(state->playedCards[i] == remodel) {        // There is a remodel in played cards.
+  for(i = 0; i < testGame->playedCardCount; i++) {
+    if(testGame->playedCards[i] == remodel) {        // There is a remodel in played cards.
       remodelPlayedCount++;
+    }
+
+    if(testGame->playedCards[i] == adventurer) {
+      adventurerPlayedCount++;
     }
   }
 
   // Player who played card.
-  if(handSizeDecrease == 2) {    // Discard remodel, trash another, gained card goes to discard pile.
+  if(state->handCount[state->whoseTurn]-2 == testGame->handCount[testGame->whoseTurn]) {    // Discard remodel, trash another, gained card goes to discard pile.
     printf(PLAYREMODEL_PASS);
   } else {
     printf(PLAYREMODEL_FAIL);
   }
-  printf("expects Player 1's 'hand' size to decrease by a total of '2', 'hand' decreased by: %d\n", handSizeDecrease);
+  printf("expects attempt to buy 'garden' by trashing a 'adventurer' to decrease handsize by a total of '2', 'hand' decreased by: %d\n", state->handCount[state->whoseTurn]-testGame->handCount[testGame->whoseTurn]);
 
-  if(actionDecrease == 1) {    // Action reduced by 1 the same.
+  if(state->handCount[state->whoseTurn]-1 == testGame->handCount[testGame->whoseTurn]) {    // Action reduced by 1 the same.
     printf(PLAYREMODEL_PASS);
   } else {
     printf(PLAYREMODEL_FAIL);
   }
-  printf("expects Player 1's 'action' amount to decrease by '1', 'actions' decreased by: %d\n", actionDecrease);
+  printf("expects attempt to buy 'garden' by trashing a 'adventurer' to decrease actions by '1', 'actions' decreased by: %d\n", state->handCount[state->whoseTurn]-testGame->handCount[testGame->whoseTurn]);
 
-  if(state->numBuys == 1) {    // Number of buys should still be 1.
+  if(state->numBuys == testGame->numBuys) {    // Number of buys should still be 1.
     printf(PLAYREMODEL_PASS);
   } else {
     printf(PLAYREMODEL_FAIL);
   }
-  printf("expects Player 1's 'buy' amount to be '1', got: %d\n", state->numBuys);
+  printf("expects attempt to buy 'garden' by trashing a 'adventurer' to keep 'buy' amount at '1', got: %d\n", testGame->numBuys);
 
   if(remodelHandCount == 0) {    // No remodel in hand.
     printf(PLAYREMODEL_PASS);
   } else {
     printf(PLAYREMODEL_FAIL);
   }
-  printf("expects Player 1 to have 0 'adventurer' cards in hand, got: %d\n", remodelHandCount);
+  printf("expects attempt to buy 'garden' by trashing a 'adventurer' to no longer have 'remodel' cards in hand, got: %d\n", remodelHandCount);
 
   if(remodelPlayedCount == 1) {    // one remodel in played cards pile.
     printf(PLAYREMODEL_PASS);
   } else {
     printf(PLAYREMODEL_FAIL);
   }
-  printf("expects Player 1 to have 1 'remodel' card in played card pile, got: %d\n", remodelPlayedCount);
+  printf("expects Player 1 to have 1 'remodel' card in played card pile after playing it, got: %d\n", remodelPlayedCount);
 
   if(remodelDiscardCount == 0) {    // 0 remodel in discard because we are trying to buy a gardens.
     printf(PLAYREMODEL_PASS);
   } else {
     printf(PLAYREMODEL_FAIL);
   }
-  printf("expects Player 1 to have 0 'remodel' cards in discard pile, got: %d\n", remodelDiscardCount);
+  printf("expects Player 1 to have 0 'remodel' cards in discard pile after playing it, got: %d\n", remodelDiscardCount);
 
   if(gardensDiscardCount == 1) {    // 1 gardens in discard because we bought a gardens.
     printf(PLAYREMODEL_PASS);
   } else {
     printf(PLAYREMODEL_FAIL);
   }
-  printf("expects Player 1 to have 1 'gardens' card in discard pile, got: %d\n", gardensDiscardCount);
+  printf("expects Player 1 to have 1 'gardens' card in discard pile after buying it, got: %d\n", gardensDiscardCount);
 
-  if(state->playedCardCount == 1) {    // Only one card in played card pile.
+  if(testGame->playedCardCount == 1) {    // Only one card in played card pile.
     printf(PLAYREMODEL_PASS);
   } else {
     printf(PLAYREMODEL_FAIL);
   }
-  printf("expects Player 1 to have 1 card in played card pile, got: %d\n", state->playedCardCount);
+  printf("expects Player 1 to have 1 card in played card pile after playing remodel, got: %d\n", testGame->playedCardCount);
 
-  // if(state->coins == 0) {             // 0 coin change.
-  //   printf(PLAYREMODEL_PASS);
-  // } else {
-  //   printf(PLAYREMODEL_FAIL);
-  // }
-  // printf("expects Player 1 'coins' to be 0 after playing adventurer, got: %d\n", state->coins);
+  if(adventurerHandCount == 0) {    // Only one card in played card pile.
+    printf(PLAYREMODEL_PASS);
+  } else {
+    printf(PLAYREMODEL_FAIL);
+  }
+  printf("expects Player 1 to have no 'adventurer' cards in hand after selecting it to trash, number of adventurers in hand: %d\n", adventurerHandCount);
 
-  // if(increasedTreasure == 2) {         // Player should have 2 more treasures in hand.
-  //   printf(PLAYREMODEL_PASS);
-  // } else {
-  //   printf(PLAYREMODEL_FAIL);
-  // }
-  // printf("expects Player 1 to have 2 additional treasures in hand after playing adventurer, got: %d\n", increasedTreasure);
+  if(adventurerDiscardCount == 0) {    // Only one card in played card pile.
+    printf(PLAYREMODEL_PASS);
+  } else {
+    printf(PLAYREMODEL_FAIL);
+  }
+  printf("expects Player 1 to have no 'adventurer' cards in discard after selecting it to trash, number of adventurers in hand: %d\n", adventurerDiscardCount);
+
+  if(adventurerPlayedCount == 0) {    // Only one card in played card pile.
+    printf(PLAYREMODEL_PASS);
+  } else {
+    printf(PLAYREMODEL_FAIL);
+  }
+  printf("expects Player 1 to have no 'adventurer' cards in played card pile after selecting it to trash, number of adventurers in played card pile: %d\n", adventurerPlayedCount);
+
 
   // Other players
   if(otherHandIncrease == 0) {    // Make sure other players hand size doesn't increase.
@@ -193,76 +211,85 @@ void testPlayRemodel() {
   printf("expects other player's 'discard' size to not increase, other player's 'discard' pile increased by: %d\n", otherDiscardIncrease);
 
 
-  // Initialize new game.
-  // Initialize game with valid game values.
-
-  // successfulInit = initializeGame(numPlayers, kingdomCards, randomSeed, state);
-  // int failPlay = PLAYREMODEL(state, state->whoseTurn);   // Don't put adventurer card in player 1's hand.
-  //
-  // if(failPlay == -1) {    // Should return error.
-  //   printf(PLAYREMODEL_PASS);
-  // } else {
-  //   printf(PLAYREMODEL_FAIL);
-  // }
-  // printf("expects method to return '-1', when player doesn't have adventurer in hand and tries to play one, got: %d\n", failPlay);
 
 
-  // // Initialize new game, test playing without deck.
-  // // Initialize game with valid game values.
-  // successfulInit = initializeGame(numPlayers, kingdomCards, randomSeed, state);
-  //
-  // // Set each card in players hand to a treasure.
-  // for(i = 0; i < state->handCount[state->whoseTurn]; i++) {
-  //   state->hand[state->whoseTurn][i] = copper;
-  // }
-  //
-  // // Set each card remaining in deck to a treasure.
-  // for(i = 0; i < state->deckCount[state->whoseTurn]; i++) {
-  //   state->deck[state->whoseTurn][i] = copper;
-  // }
-  //
-  // // Draw entire deck and discard each card. Sets deck to empty.
-  // for(i = 0; i < state->deckCount[state->whoseTurn]; i++) {
-  //   drawCard(state->whoseTurn, state);              // Draw card.
-  //   discardCard(i, state->whoseTurn, state, 0);     // Discard card.
-  // }
-  // state->hand[state->whoseTurn][0] = adventurer;    // Turn first card in hand to adventurer.
-  // failPlay = PLAYREMODEL(state, state->whoseTurn);          // Play adventurer with empty deck.
-  //
-  // if(failPlay == 0) {    // Should be successful.
-  //   printf(PLAYREMODEL_PASS);
-  // } else {
-  //   printf(PLAYREMODEL_FAIL);
-  // }
-  // printf("expects method to return '0', when player plays adventurer with no cards in deck, got: %d\n", failPlay);
-  //
-  // // Since each card in players hand and deck was set to treasure, there should be cards remaning in deck after playing adventure.
-  // if(state->deckCount[state->whoseTurn] > 0) {
-  //   printf(PLAYREMODEL_PASS);
-  // } else {
-  //   printf(PLAYREMODEL_FAIL);
-  // }
-  // printf("expects deck to contain more than '0', after player plays adventurer with no cards in deck, got: %d\n", state->deckCount[state->whoseTurn]);
-  //
-  // if(state->discardCount[state->whoseTurn] == 0) {
-  //   printf(PLAYREMODEL_PASS);
-  // } else {
-  //   printf(PLAYREMODEL_FAIL);
-  // }
-  // printf("expects discard to contain '0' cards, after player plays adventurer with no cards in deck, got: %d\n", state->discardCount[state->whoseTurn]);
-  //
+  // Try buying a card thats to expensive
+  memcpy(testGame, state, sizeof(struct gameState));                    // Setup clean test game.
+
+  // Set player 1's first card in hand to be adventurer card.
+  testGame->hand[testGame->whoseTurn][0] = remodel;
+  testGame->hand[testGame->whoseTurn][1] = copper;
+
+  int succussfulRemodel = playRemodel(testGame, testGame->whoseTurn, 0, 1, 7);      // Trying to buy adventurer by trashing a copper.
+
+  if(testGame->handCount[testGame->whoseTurn] == state->handCount[state->whoseTurn]) {    // Make sure hand count stays the same.
+    printf(PLAYREMODEL_PASS);
+  } else {
+    printf(PLAYREMODEL_FAIL);
+  }
+  printf("expects attempt to buy 'adventurer' by trashing a 'copper', to not change hand size, hand size changed by: %d\n", testGame->handCount[testGame->whoseTurn]-state->handCount[state->whoseTurn]);
+
+  if(testGame->deckCount[testGame->whoseTurn] == state->deckCount[state->whoseTurn]) {    // Make sure deck count stays the same.
+    printf(PLAYREMODEL_PASS);
+  } else {
+    printf(PLAYREMODEL_FAIL);
+  }
+  printf("expects attempt to buy 'adventurer' by trashing a 'copper', to not change deck size, deck size changed by: %d\n", testGame->deckCount[testGame->whoseTurn]-state->deckCount[state->whoseTurn]);
+
+  if(testGame->discardCount[testGame->whoseTurn] == state->discardCount[state->whoseTurn]) {    // Make sure discard count stays the same.
+    printf(PLAYREMODEL_PASS);
+  } else {
+    printf(PLAYREMODEL_FAIL);
+  }
+  printf("expects attempt to buy 'adventurer' by trashing a 'copper', to not change discard pile size, discard pile size changed by: %d\n", testGame->discardCount[testGame->whoseTurn]-state->discardCount[state->whoseTurn]);
+
+  if(succussfulRemodel == -1) {    // Make sure other players discard size doesn't increase.
+    printf(PLAYREMODEL_PASS);
+  } else {
+    printf(PLAYREMODEL_FAIL);
+  }
+  printf("expects attempt to buy 'adventurer' by trashing a 'copper', to return '-1', got: %d\n", succussfulRemodel);
 
 
 
 
+  // Try buying a card thats the exact amount affordable amount, garden is 4 so is feast.
+  memcpy(testGame, state, sizeof(struct gameState));                    // Setup clean test game.
+
+  // Set player 1's first card in hand to be adventurer card.
+  testGame->hand[testGame->whoseTurn][0] = remodel;
+  testGame->hand[testGame->whoseTurn][1] = gardens;
+
+  succussfulRemodel = playRemodel(testGame, testGame->whoseTurn, 0, 1, 9);      // Trying to buy feast by trashing a garden.
+
+  if(testGame->handCount[testGame->whoseTurn] == state->handCount[state->whoseTurn]-2) {    // Make sure hand count stays the same.
+    printf(PLAYREMODEL_PASS);
+  } else {
+    printf(PLAYREMODEL_FAIL);
+  }
+  printf("expects attempt to buy 'feast' by trashing a 'garden', to decrease handsize by 2, hand size changed by: %d\n", testGame->handCount[testGame->whoseTurn]-state->handCount[state->whoseTurn]);
+
+  if(testGame->deckCount[testGame->whoseTurn] == state->deckCount[state->whoseTurn]) {    // Make sure deck count stays the same.
+    printf(PLAYREMODEL_PASS);
+  } else {
+    printf(PLAYREMODEL_FAIL);
+  }
+  printf("expects attempt to buy 'feast' by trashing a 'garden', to not increase deck size, deck size changed by: %d\n", testGame->deckCount[testGame->whoseTurn]-state->deckCount[state->whoseTurn]);
+
+  if(testGame->discardCount[testGame->whoseTurn] == state->discardCount[state->whoseTurn]+2) {    // Make sure discard count stays the same.
+    printf(PLAYREMODEL_PASS);
+  } else {
+    printf(PLAYREMODEL_FAIL);
+  }
+  printf("expects attempt to buy 'feast' by trashing a 'garden', to increase discard pile size by 2, discard pile size changed by: %d\n", testGame->discardCount[testGame->whoseTurn]-state->discardCount[state->whoseTurn]);
 
 
+  // Clear memory
+  free(state);
+  state = 0;
 
-
-
-
-
-
+  free(testGame);
+  testGame = 0;
 
 }
 

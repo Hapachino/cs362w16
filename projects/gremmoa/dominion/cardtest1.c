94737bcd70015e224de728576f5a46a5a466fbd1
Andrew Gremmo <andrewjgremmo@gmail.com>
2016-01-31 12:28:29 -0500

    Add unit test for playSmithy and fix card receive count bug for playSmithy

---
+++ projects/gremmoa/dominion/cardtest1.c
@@ -0,0 +1,66 @@
+/* Test for playSmithy
+ * Requirements
+ * 1. Current player receives 3 cards
+ * 2. 3 Cards come from players own pile
+ * 3. No state change should occur for other players
+ * 4. No state change should occur to the victory/kingdom card piles
+ */
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+#include <stdlib.h>
+
+#define TESTCARD "smithy"
+
+int main() {
+  int i;
+  int cardTypes = 27;
+  int newCards = 3;
+  int discarded = 1;
+
+  int handpos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
+  int seed = 1000;
+  int numPlayers = 2;
+  int thisPlayer = 0;
+  int otherPlayer = 1;
+  struct gameState G, testG;
+  int k[10] = {adventurer, smithy, council_room, remodel, great_hall, mine,
+      baron, feast, gardens, village};
+
+  initializeGame(numPlayers, k, seed, &G);
+
+  printf("----------------- Testing Card: %s ----------------\n", TESTCARD);
+
+  // copy the game state to a test case
+  memcpy(&testG, &G, sizeof(struct gameState));
+  cardEffect(smithy, choice1, choice2, choice3, &testG, handpos, &bonus);
+
+  printf("--- Current Player ---\n");
+  printf("hand count = %d, expected = %d\n", testG.handCount[thisPlayer], G.handCount[thisPlayer] + newCards - discarded);
+  printf("deck count = %d, expected = %d\n", testG.deckCount[thisPlayer], G.deckCount[thisPlayer] - newCards);
+
+  printf("--- Other Player ---\n");
+  printf("hand count = %d, expected = %d\n", testG.handCount[otherPlayer], G.handCount[otherPlayer]);
+  printf("deck count = %d, expected = %d\n", testG.deckCount[otherPlayer], G.deckCount[otherPlayer]);
+
+  printf("--- Kingdom/Victory Pile ---\n");
+  for (i = 0; i < cardTypes; i++) {
+    printf("Card type %d count = %d, expected = %d\n", i, testG.supplyCount[i], G.supplyCount[i]);
+    assert(testG.supplyCount[i] == G.supplyCount[i]);
+  }
+  assert(testG.handCount[thisPlayer] == G.handCount[thisPlayer] + newCards - discarded);
+  assert(testG.deckCount[thisPlayer] == G.deckCount[thisPlayer] - newCards);
+  assert(testG.handCount[otherPlayer] == G.handCount[otherPlayer]);
+  assert(testG.deckCount[otherPlayer] == G.deckCount[otherPlayer]);
+  assert(testG.supplyCount[estate] == G.supplyCount[estate]);
+  assert(testG.supplyCount[duchy] == G.supplyCount[duchy]);
+  assert(testG.supplyCount[province] == G.supplyCount[province]);
+
+  printf("\n >>>>> SUCCESS: Testing complete %s <<<<<\n\n", TESTCARD);
+
+  return 0;
+}

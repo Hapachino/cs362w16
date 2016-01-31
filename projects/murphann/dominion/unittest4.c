b97e5f4b27b029dc5cc9d8656b4deb2425ed2bc2
Anna Murphy <murphann@oregonstate.edu>
2016-01-30 19:50:15 -0800

    all unit tests

--- projects/murphann/dominion/unittest4.c
+++ projects/murphann/dominion/unittest4.c
@@ -6,7 +6,7 @@
 #include "rngs.h"
 #include <stdlib.h>
 
-#define TESTMETHOD ""
+#define TESTMETHOD "drawCard"
 
 int main() {
   int numPlayers = 2;
@@ -17,11 +17,19 @@ int main() {
   int kcards[10] = {adventurer, minion, cutpurse, steward, smithy,
                council_room, village, mine, tribute, ambassador};
 
-   initializeGame(numPlayers, kcards, seed, &G);
+  initializeGame(numPlayers, kcards, seed, &G);
 
-   printf("********Testing %s***********\n", TESTMETHOD);
+  printf("********Testing %s***********\n", TESTMETHOD);
 
-   memcpy(&storeG, &G, sizeof(struct gameState));
+  memcpy(&storeG, &G, sizeof(struct gameState));
 
-   return 0;
+  drawCard(p1, &G);
+
+  printf("Expected hand size: %d  New Hand Size: %d\n",
+           storeG.handCount[p1]+1, G.handCount[p1]);
+
+  printf("Expected Deck Size: %d  New Deck Size: %d\n",
+          storeG.deckCount[p1]-1, G.deckCount[p1]);
+
+  return 0;
 }

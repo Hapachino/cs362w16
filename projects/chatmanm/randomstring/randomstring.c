19a424808cb82a5c93f9d5eb6d493c69b86d320b
Myles Chatman <myleschatman@gmail.com>
2016-02-01 18:19:29 -0800

    Create randomstring.c

---
+++ projects/chatmanm/randomstring/randomstring.c
@@ -0,0 +1,8 @@
+Myles Chatman
+CS 362 Software Engineering II
+1/31/2016
+
+inputChar()
+As the assignment specified, it mentioned to generate a random character so I implemented a simple return statement 
+
+inputString()

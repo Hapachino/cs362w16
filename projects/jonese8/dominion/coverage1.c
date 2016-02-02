3056cf3ab700867e39c14e0f3be1779eb224e6d9
jonese8 <jonese8@onid.oregonstate.edu>
2016-02-01 20:41:54 -0700

    Added Assignment 3 files

---
+++ projects/jonese8/dominion/coverage1.c
@@ -0,0 +1,11 @@
+CS362 - Assignment 3 - Emmalee Jones
+02/02/2016
+
+File 'dominion.c'
+Lines executed:39.97% of 578
+Branches executed:48.68% of 417
+Taken at least once:39.81% of 417
+Calls executed:26.80% of 97
+dominion.c:creating 'dominion.c.gcov'
+
+

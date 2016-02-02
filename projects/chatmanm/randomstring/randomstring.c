e359abf368d0829d6cfcc71afe56e4e9eacfc275
Myles Chatman <myleschatman@gmail.com>
2016-02-01 18:29:12 -0800

    Update randomstring.c

--- projects/chatmanm/randomstring/randomstring.c
+++ projects/chatmanm/randomstring/randomstring.c
@@ -3,6 +3,13 @@ CS 362 Software Engineering II
 1/31/2016
 
 inputChar()
-As the assignment specified, it mentioned to generate a random character so I implemented a simple return statement 
+As the assignment specified, it mentioned to generate a random character so I implemented a simple return statement that
+returns letters.
 
 inputString()
+Since the program needs a variable that contains the characters 'r,e,s,t' to produce the error, I stored those values in an
+array along with temporary string array. Entering the for loop, values from the string array will be stored in the temporary
+string array. Order of the character does not matter, as long as it contains those values, the program will exit.
+
+Sometimes the test takes a very long time and there are other times it may be quick. It really just depends on the
+different possiblities of combinations. For example, one test took 1,567,351 iterations and another test took 657,378.

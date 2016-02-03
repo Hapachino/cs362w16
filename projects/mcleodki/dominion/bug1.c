3fcdfad69eddef0c31a3e06144ad495895bf74bb
CaptainKimothy <mcleodki@onid.oregonstate.edu>
2016-02-03 01:21:04 -0600

    added bug1.c

---
+++ projects/mcleodki/dominion/bug1.c
@@ -0,0 +1,8 @@
+/* Author: Kim McLeod
+ * Assignment 3
+ * CS362 W16
+ * bug1.c
+
+  The only bug I encountered during testing was when I forgot what I had altered in the dominion.c file. I forgot that some of the tests were supposed to fail due to the bad code that I was feeding it. Luckily, I realized quickly that it was a good thing that they were failing. Realizing this, I was better able to make sure the tests were catching what they were supposed to.
+
+*/

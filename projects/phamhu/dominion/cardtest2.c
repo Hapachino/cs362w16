40827ae393114a40c204dd1c26dd9c337016c1dd
Huy Pham <phamhu@onid.oregonstate.edu>
2016-02-02 18:24:09 -0800

    Completed all cardtests.

--- projects/phamhu/dominion/cardtest2.c
+++ projects/phamhu/dominion/cardtest2.c
@@ -1,10 +1,10 @@
 /* -----------------------------------------------------------------------
- * Testing updateCoins() method - using testUpdateCoins.c as the base code.
+ * Testing adventurer card - using testUpdateCoins.c as the base code.
  *
  * Include the following lines in your makefile:
  *
- * unittest1: unittest1.c dominion.o rngs.o
- *      gcc -o unittest1 -g  unittest1.c dominion.o rngs.o $(CFLAGS)
+ * unittest1: cardtest2.c dominion.o rngs.o
+ *      gcc -o cardtest2 -g  cardtest2.c dominion.o rngs.o $(CFLAGS)
  * -----------------------------------------------------------------------
  */
 
@@ -16,31 +16,48 @@
 #include "rngs.h"
 
 int main() {
-    int i;
-    int seed = 1000;
-    int numPlayer = 2;
-    int maxBonus = 10;
-    int p, r, handCount;
-    int bonus;
+    int seed = rand() % 1000;
+    int numPlayers = 2;
+    int p, player, round;
     int k[10] = {adventurer, council_room, feast, gardens, mine
                , remodel, smithy, village, baron, great_hall};
-    struct gameState G;
+    struct gameState oldGame;
+	struct gameState newGame;
     int maxHandCount = 5;
     int failCount = 0;
     int passCount = 0;
-    // arrays of all coppers, silvers, and golds
-    int coppers[MAX_HAND];
-    int silvers[MAX_HAND];
-    int golds[MAX_HAND];
-    for (i = 0; i < MAX_HAND; i++)
-    {
-        coppers[i] = copper;
-        silvers[i] = silver;
-        golds[i] = gold;
-    }
 
-    printf ("TESTING cardtest2():\n");
+    printf ("<< TESTING cardtest2 - adventurer  >>\n");
+	
+	for(p = 0; p < maxHandCount; p++)
+	{
+		memset(&oldGame, 23, sizeof(struct gameState));         //clear the game state
+		memset(&newGame, 23, sizeof(struct gameState));         //clear the game state
+		initializeGame(numPlayers, k, seed, &newGame);
+		for(round = 0; round < 2; round++)
+		{
+			for(player = 0; player < numPlayers; player++)
+			{
+				memcpy(&oldGame, &newGame, sizeof(struct gameState));
+				adventurerCard(player, &newGame);
+/*
+ 				printf("Old stuff - player %d\n", player);
+				printf("deckCount: %d handCount: %d\n", oldGame.deckCount[player], oldGame.handCount[player]);
+				printf("New stuff\n");				
+				printf("deckCount: %d handCount: %d\n", newGame.deckCount[player], newGame.handCount[player]);
+*/
+				if((newGame.handCount[player] == oldGame.handCount[player] + 2) && (newGame.deckCount[player] == oldGame.deckCount[player] - 2))
+				{
+					passCount++;
+				}
+				else
+				{
+					failCount++;
+				}
 
+			}	
+		}		
+	}
     printf("Number of cases passed: %d\n", passCount);
     printf("Number of cases failed: %d\n", failCount);
 

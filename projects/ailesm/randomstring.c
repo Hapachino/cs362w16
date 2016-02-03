6d8f7bdcc50f0e9a43674ddcc79f715c8d4ce4e5
Matthew Ailes <mattailes@gmail.com>
2016-02-02 19:53:01 -0500

    update

--- projects/ailesm/randomstring.c
+++ projects/ailesm/randomstring.c
@@ -4,5 +4,6 @@ For the inputChar() I just randomly assigned the char value to a random number t
 I did the same for inputString(), but just had it go through a for loop to add it five times and limited the characters to lower case,
 so as to keep the randomness down a bit.
 
-This caused the test function to run about 1.5 million times before exiting with the error. This really shows off how bad random
-testing can be.
+This caused the test function to run about 1.5 million times the first time I ran it before exiting with the error. The sedond run was
+reaching almost 3 million iterations before I decided to just stop the program.
+This really shows off how bad random testing can be.

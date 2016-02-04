b5bfb6505d6a7afc246d0eaad756d3caac060371
Nancy Chan <nancychannc@gmail.com>
2016-02-04 15:53:51 -0800

    Initial commit

---
+++ projects/channa/dominion/randomhistory.c
@@ -0,0 +1,9 @@
+Nancy Chan
+CS 362
+Winter 2016
+Assignment 4: randomhistory.c 
+
+randomtestadventurer:
+	
+    -: 1258:      //if the deck is empty we need to shuffle discard and add to deck
+   20: 1259:      shuffle(currentPlayer, state);

310a61dda7bdc53e4015ca5c18729a43a2ff4892
Kendra Swafford <swaffork@flip1.engr.oregonstate.edu>
2016-02-12 18:37:08 -0800

    More skeleton

--- projects/swaffork/dominion/randomtestcard.c
+++ projects/swaffork/dominion/randomtestcard.c
@@ -1,1 +1,8 @@
-some text
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include "testhelper.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>

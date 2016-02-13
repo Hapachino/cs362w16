2e3242630425488199c006c59e5190ac643826b5
Kendra Swafford <swaffork@flip1.engr.oregonstate.edu>
2016-02-12 18:36:06 -0800

    randomtestadventurer.c

--- projects/swaffork/dominion/randomtestadventurer.c
+++ projects/swaffork/dominion/randomtestadventurer.c
@@ -6,3 +6,47 @@
 #include <string.h>
 #include <stdio.h>
 #include <assert.h>
+
+int setUp(struct gameState* state)
+{
+    int player = 0;
+    int seed = 1000;
+    int numPlayers = 2;
+    int cards[10] = {adventurer, council_room, feast, gardens, mine,
+                     remodel, smithy, village, baron, great_hall};
+
+    // Initialize gameState
+    if (initializeGame(numPlayers, cards, seed, state) != 0)
+    {
+        printf("Error: Default initializeGame() failed.\n");
+        return -1;
+    }
+    
+    state->whoseTurn = player;
+    return 0;
+}
+
+int testAdventurer(struct gameState* state)
+{
+    int player = 0;
+    int rand = 0;
+    int i;
+    struct gameState state;
+    if (setUp(&state) == -1) return 0;
+
+    // Randomize player deck
+    state->deckCount[player] = 0;
+    state->deckCount[player + 1] = 0;
+    rand = rand() % MAX_DECK;
+    
+    for (i = 0; i < rand; i++)
+    {
+    }
+
+    return 0;
+}
+
+int main()
+{
+    srand(time(NULL));
+

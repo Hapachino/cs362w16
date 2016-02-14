2a913dee9a751c1fe365c726952c4e2656296f82
gariepyt <tom@tomgariepy.com>
2016-02-14 02:18:36 -0800

    Completed work for Assignment 4
    Created random tests and wrote description

--- projects/gariepyt/dominion/randomhistory.c
+++ projects/gariepyt/dominion/randomhistory.c
@@ -1,6 +1,23 @@
 /*
+Random Test Card:
+	Tests the salvager
+	Calculated seed by using time(null)
+	randomly assigns:
+		number of players
+		current player
+		number of cards in hand
+		number of cards in trash
+	did force the code to run until an error was found
+		changed once it became apparent that an error would be found on the first try
+		Now runs 100 times
 
-
-
-
+Random Test Adventurer:
+	Calculated seed by using time(null)
+	randomly assigns:
+		number of players
+		current player
+		number of cards in hand
+	did force the code to run until an error was found
+		changed once it became apparent that an error would be found on the first try
+		Now runs 100 times
 */

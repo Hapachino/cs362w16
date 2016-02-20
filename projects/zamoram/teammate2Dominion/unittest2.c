e290d3ca10868d10307ed9860718322faa105fc7
Marco Zamora <marco.zamora@comcast.net>
2016-02-19 23:32:27 -0600

    Teammate 2 files
    Renamed the teammate's test and added my tests.

--- projects/zamoram/teammate2Dominion/unittest2.c
+++ projects/zamoram/teammate2Dominion/unittest2.c
@@ -1,68 +1,86 @@
 /* -----------------------------------------------------------------------
- * getCost() Unit Testing file
- * Author: James Linnenburger - Oregon State CS362 Spring 2016
- *
- * This test calls the getCost method for all possible cards in the game
- * and check to make sure it returns the correct values.
- * -----------------------------------------------------------------------
- */
+	* Testing shuffle()
+	* This function shuffles cards in players deck
+	* -----------------------------------------------------------------------
+*/
 
 #include "dominion.h"
 #include "dominion_helpers.h"
 #include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <assert.h>
 #include "rngs.h"
+#include <math.h>
 
-
-
-
-
-// set NOISY_TEST to 0 to remove printfs from output
-#define NOISY_TEST 1
-
+int checkShuffle(int player, struct gameState *post)
+{
+	struct gameState pre;
+	memcpy(&pre, post, sizeof(struct gameState));
+	
+	int result = shuffle(player, post);
+	
+	if(result != 0)
+	{
+		printf("ERROR: shuffle function failed.\n\n");
+	}
+	//checking that deck is now different and no card has been lost
+	else if(pre.deckCount[player] != post->deckCount[player])
+	{
+		printf("ERROR: Deck counts do not match.\n\n");
+	}
+	
+	else if(memcmp(&pre.deck[player], post->deck[player], sizeof(int) * pre.deckCount[player]) == 0)
+	{
+		printf("ERROR: Deck was not shuffled.\n");
+	}
+	//assert(memcmp(&pre.deck[player], post->deck[player], sizeof(int) * pre.deckCount[player]) != 0);
+	
+	return 0;
+}
 
 int main()
 {
-    int cardCosts[27] = {0, 2, 5, 8, 0, 3, 6, 6, 5, 4, 4, 5, 4, 4, 3, 4, 3, 5, 3, 5, 3, 4, 2, 5, 4, 4, 4};
-    char *cardNames[27] =  {"curse", "estate", "duchy", "province", "copper",
-                            "silver", "gold", "adventurer", "council_room", "feast",
-                            "gardens", "mine", "remodel", "smithy", "village", "baron",
-                            "great_hall", "minion", "steward", "tribute",
-                            "ambassador", "cutpurse", "embargo", "outpost", "salvager",
-                            "sea_hag", "treasure_map"};
-
-    int i, pass = 0, fail = 0;
-
-    printf ("\nTESTING getCost():\n");
-
-    //test all possible card costs and an out of range value
-    for(i = 0; i < 28; i++)
-    {
-        if(cardCosts[i] == getCost(i))
-        {
-            pass++;
-            #if (NOISY_TEST == 1)
-            printf("\tCard: %s Passed.\n", cardNames[i]);
-            #endif
-        }
-        else if(getCost(i) == -1 && (i < 0 || i > 26)) //was an invalid input sent? should return -1
-        {
-            pass++;
-            #if (NOISY_TEST == 1)
-            printf("\tNot a Card: Passed.\n");
-            #endif
-        }
-        else
-        {
-            #if (NOISY_TEST == 1)
-            fail++;
-            printf("\tCard: %s failed.\n", cardNames[i]);
-            #endif
-        }
-    }
-
-    printf("Test results: [%d fails]  [%d/28 passes]\n          >>>>> Testing getCost() Complete <<<<<\n\n", fail, pass);
-    return 0;
+	struct gameState G; 
+	//cards in player decks
+	int numPlayers; //players
+	int p; //current player
+	int i, j;
+	int count; //deck count
+	
+	int k[10] = {adventurer, council_room, feast, gardens, mine
+		   , remodel, smithy, village, baron, great_hall};
+	
+	SelectStream(2);
+	PutSeed(3);
+	printf("Testing shuffle() unittest2.\n");
+	
+	 for(numPlayers = 2; numPlayers <= MAX_PLAYERS; numPlayers++) //tests up to max # of players
+	 {
+		for(p = 0;  p < numPlayers; p++) //test for each player currently playing
+		{
+			//test different deck amounts
+			//not testing < 4 since its more likely that shuffled decks won't change
+			for(count = 4; count < 50; count++) 
+			{
+				for (i = 0; i < sizeof(struct gameState); i++) { //from the lessons, random gameState
+					((char*)&G)[i] = floor(Random() * 256);
+				}
+				
+				//fill in some real cards
+				for(j = 0; j < count; j++)
+				{
+					int card = floor(Random() * 10);
+					G.deck[p][j] = k[card];
+				}
+				//set other necessary state settings
+				G.numPlayers = numPlayers;
+				G.deckCount[p] = count;
+				
+				checkShuffle(p, &G);
+			}
+		}
+	 }
+	 printf ("SHUFFLE TESTS FINISHED\n\n");
+	
+	return 0;
 }

f466fe92fe5e8a648c064dac25ad89addd58a461
Nancy Chan <nancychannc@gmail.com>
2016-02-23 14:05:13 -0800

    Initial commit

---
+++ projects/channa/dominion/BugsInTeammates.c
@@ -0,0 +1,42 @@
+Nancy Chan
+CS 362
+Winter 2016
+Assignment 5: BugsInTeammates.c
+
+teammate1Dominion - Andrew Calhoun
+
+	unittest1: updateCoins()
+		All tests passed.
+
+	unittest2: fullDeckCount()
+
+	unittest3: isGameOver()
+		All tests passed.
+
+	unittest4: scoreFor()
+
+	cardtest1: smithy
+
+	cardtest2: adventurer
+
+	cardtest3: cutpurse
+
+	cardtest4: remodel
+
+teammate2Dominion - Jonathan Lagrew
+
+	unittest1: updateCoins()
+
+	unittest2: fullDeckCount()
+
+	unittest3: isGameOver()
+
+	unittest4: scoreFor()
+
+	cardtest1: smithy
+
+	cardtest2: adventurer
+
+	cardtest3: cutpurse
+
+	cardtest4: remodel

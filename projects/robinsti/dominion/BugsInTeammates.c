d522d9aa2ec3b1ac4b2009a2eae35ab6625e301c
Tim Robinson <robinsti@engr.orst.edu>
2016-02-27 15:14:33 -0800

    Updated bug report

--- projects/robinsti/dominion/BugsInTeammates.c
+++ projects/robinsti/dominion/BugsInTeammates.c
@@ -1,30 +1,113 @@
-/******************************************************************************
+/*****
  ** Author: Tim Robinson
  ** Date created: 2/27/2015
+ ** CS 362, Oregon State University
 
-Isaiah (perrotti)
------------------
-The bugs that I found were in the Adventurer card. The results of the tests
-can be seen in the robinsti_unittestresults.out file, the tests for which I 
-replaced a failed assertion with if and printf statement. For the adventurer 
-card, we can see that the card counts are off for both the cards in hand for 
-the player and the cards the player has in their possession in total.
+===============================================================================
+Isaiah (perotti) Report
+===============================================================================
 
-Specifically, we have:
+Title:
+Playing Adventurer causes player to draw an additional treasure card.
 
-The player always has 1 more card than expected in their hand.
-The player always has 1 fewer card than expected in their possession in total.
+Class:
+Gameplay bug
 
-Will (thomasw)
+Date:  2/27/2016         
+Reported By:	Tim Robinson
+Email:	robinsti@oregonstate.edu
+
+
+Product: Dominion
+Platform: Mac OSX 	Version: 10.11.3
+
+Is it reproducible: Yes
+
+Description
+===========
+When adventurer card is played, the player draws an additional treasure card.
+
+
+Steps to Produce/Reproduce
+--------------------------
+1. Initialize 2-player game of Dominion.
+2. Play adventurer card as first action (call adventurer function)
+3. Count the players hand, it will have 1 more than expected.
+
+
+Expected Results
+----------------
+Player draws 2 treasure cards from their deck.
+
+
+Actual Results
 --------------
+Player draws 3 tresure cards from their deck.
 
-The bugs that I found were also in the refactored Adventurer card function for
-Will's code. As can be seen in the robinsti_unittestresults.out file, the
-adventurer card function causes the total number of cards in the player's
-possession to have fewer cards than expected.
 
-Specifically, we have:
 
-The player always has 1 fewer card than expected in their possession in total.
+Workarounds
+-----------
+None
+
+
+Other information
+-----------------
+Test results can be seen in robinsti_unittestresults.out
+
+===============================================================================
+Will (thomasw) Report
+===============================================================================
+
+
+Title: 
+Playing adventurer causes player to have 1 fewer card in their possession than 
+expected.
+
+Class:
+Gameplay bug
+
+Date: 2/27/2016
+Reported By: Tim Robinson
+Email: robinsti@oregonstate.edu
+
+Product: Dominion
+Platform: Mac OSX 	Version: 10.11.3
+
+Is it reproducible: Yes
+
+Description
+===========
+The player does not discard the expected number of non-treasure cards drawn.
+
+
+Steps to Produce/Reproduce
+--------------------------
+1. Initialize 2-player game of Dominion.
+2. Play adventurer card as first action (call adventurer function)
+3. Count the cards in the player's hand, discard pile and deck. A card has
+   disappeared.
+
+
+Expected Results
+----------------
+Player has 2 additional cards in their hand.
+Player's total number of cards remains constant.
+
+
+Actual Results
+--------------
+Player has 2 additional cards in their hand.
+PLayer has 1 fewer card than previously.
+
+
+Workarounds
+-----------
+None
+
+
+Other Information
+-----------------
+Test results can be seen in robinsti_unittestresults.out
 
  *****************************************************************************/

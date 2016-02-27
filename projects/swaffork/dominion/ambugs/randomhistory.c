5bae0f66fd0a96149aca993c03b3dd35e0cc8151
Kendra Swafford <swaffork@flip3.engr.oregonstate.edu>
2016-02-27 11:14:20 -0800

    still more organizing

--- projects/swaffork/dominion/ambugs/randomhistory.c
+++
@@ -1,5 +0,0 @@
-/*
-  Using the random seed I generated I wanted to focus on testing on diffrent starting conditions, so I randomized the seed which changes the diffrent starting conditions for each of the decks. This means that diffrent cards will be delbt each time for which will create diffrent starting conditions for each time the test is run.
-
-  For the adventurer card this means that there are a difrent number of money cards in diffrent locations in the deck. This brought certain problems to light such as how the number of cards will never be discarded no matter how big the deck. There is also an error that somtimes shows up where the coins are not correct, but it does not show up on every test so it could be missed without random testing.
-*/

d9b258bc76913f4192a739abe714719f5eac2190
gregoryj98 <whitegr@oregonstate.edu>
2016-02-28 09:57:47 -0700

    BugFixes.c Update.

--- projects/whitegr/BugFixes.c
+++ projects/whitegr/BugFixes.c
@@ -1,7 +1,7 @@
 Bug Fixes: My bugs were basic and simple to find -
 
 Adventure:
-Incorrect # of treasures added to player's hand.
+Incorrect # of treasures added to player's hand. Treasure count variable incorrectly initilized.
 
 Smithy:
-Incorrect # of draws occur. 
+Incorrect # of draws occur. Caused by incorrect loop condition.

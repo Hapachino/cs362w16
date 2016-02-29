4ad33e5770fd5d3f5c80cf01b000d283d5d5b3a8
Anna Murphy <murphann@oregonstate.edu>
2016-02-28 20:03:21 -0800

    Added bug reports for teammates

---
+++ projects/murphann/garciaan/BugsInGarciaans.c
@@ -0,0 +1,8 @@
+/*
+  Bugs In Play Adventurer:
+    -Deals extra Cards
+    -does not correctly discard non-tresure Cards
+    -Draws more than two Tresure
+
+Discoverd by running my random tests and unit and card tests
+*/
---
+++ projects/murphann/swaffork/BugsInSwaffork.c
@@ -0,0 +1,8 @@
+/*
+  Bugs Found In Adventurer:
+    -Program has a Segmentation Fault whenever the Adventurer card is played
+
+  Bugs Found In Smithy:
+    -Incorrect number of cards in hand and Discard pile after Smithy is played
+
+*/

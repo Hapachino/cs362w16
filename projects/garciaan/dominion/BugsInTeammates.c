d38fad748facc9547259029c88723013427e03ff
Andy Garcia <garciaan@oregonstate.edu>
2016-02-28 17:35:51 -0800

    Modified the bugs in teammates file.

--- projects/garciaan/dominion/BugsInTeammates.c
+++ projects/garciaan/dominion/BugsInTeammates.c
@@ -13,31 +13,8 @@ FAILED TESTS:
 	- Played Card Effects
 - Adventurer
 	- All Tests
+	- Suspected to start within the hand effects
 
----- Bug Fixes ----
--- Smithy --
-- Cause: Discard immediately after draw each time
-- Fix: Move discard after drawing 3 cards and only do once
-- Result: Hand effects still fail
-- Cause: Test accidentally tested for deck result, not hand result
-- Fix: Searched hand, not deck for the drawn cards
-- Result: All passing
-
--- Adventurer --
-- Cause: the drawn card is not known, so there is a semi infinite loop
-because it cannot tell if a coin is drawn and therefore discards it
-immediately
-- Fix: set the cardDrawn variable to the last card drawn
-- Result: Hand Effects and played card effects still fail
-- Cause: Adventurer does not get discarded after played
-- Fix: Added hand position parameter to function to be 
-able to discard the adventurer card from hand
-- Result: Scenario of No treasure in deck still fails
-- NOTE: Known bug with original dominion fail
-- Cause: Infinite loop caused by function not knowing that
-it has gone through all the cards
-Fix: Checked if discard pile was empty before shuffle attempt
-Result: All passing
 
 ----------------- TEAMMATE 2 -----------------
 TESTED: 

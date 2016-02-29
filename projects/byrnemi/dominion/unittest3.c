2444436e472fa8fe4a445dd3b0a8fac999b3b09f
Michael Byrne <byrnemi@flip3.engr.oregonstate.edu>
2016-02-28 21:42:24 -0800

    adding assignment 5

--- projects/byrnemi/dominion/unittest3.c
+++ projects/byrnemi/dominion/unittest3.c
@@ -1,68 +1,138 @@
-//*********************************************
-//a program to test the function handCard() from
-//dominion as implemented in dominion.c using
-//randomized inputs.
-//Author: Michael Byrne, OSU byrnemi
-//For: CS 362 Software Engineering 2
-//Sources: na
-//*********************************************
-
-#include "dominion.h"
-#include "dominion_helpers.h"
+/* unittest3.c
+ * Author: Eric Anderson
+ * Class: CS362w16
+ * Description: Unit test for the playCard function in dominion.c
+ *  Play Card gets the card played and checks it to see if the 
+ *  selected card is an action, it then "palys" the card through 
+ *  cardEffect. Then it reduces number of actions and ends by 
+ *  updating coins (Treasure cards may be added with card draws).
+*/
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #include <assert.h>
+#include "dominion.h"
+#include "dominion_helpers.h"
 #include "rngs.h"
 
-int main () {
+#define DEBUG 0
+#define NOISY_TEST 0
 
-    int r;
-    int errors = 0;
-    int k[10] = {adventurer, council_room, feast, gardens, mine,
-           remodel, smithy, village, baron, great_hall};
+/**********************************************************
+ * function: checkPlayCard
+ * description: This function executes the checkPlayCard function
+ * against several conditions conditions;
+ *    checks if the state->phase != 0
+ *    checks if the state->numActions < 1
+ *    check if selected card is an adventurer or treasure_map
+ *    checks if playing the card failed
+ * in any of these cases the function will return a -1.  This tests for both cases.
+ * input: the player number (0-3), a prefilled out struct and a card number
+ * returns: result, 0 for success, 1 for failure
+ ***********************************************************/
+int checkPlayCard(int handPos, int c1, int c2, int c3, struct gameState *post)
+{
+  //define variables
+  struct gameState pre;
+  memcpy (&pre, post, sizeof(struct gameState));
 
-    struct gameState G;
+  int card = handCard(handPos, post);
+  int coin_bonus = 0;
+  int r = 0;
+  int result = 0;
 
-    r = initializeGame(2, k, 1, &G);
-    SelectStream(2);
-    PutSeed(3);
+  // execute the playCard action against the "pre" version of the struct
+  // this is the same content as in the playCard code in dominion.c
+  r = playCard(handPos, c1, c2, c3, post);
+  
+  // test 1: Check for the four conditions and make sure we got a -1
+  if (pre.phase != 0 || pre.numActions < 1 || card < adventurer || card > treasure_map ||
+      cardEffect(card, c1, c2, c3, &pre, handPos, &coin_bonus) < 0) 
+  {
+#if (DEBUG)
+    assert (r == -1);
+#endif
 
-    printf ("initializeGame(4, k, 1, &G) = %d\n", r);
-    assert(r == 0);
+#if (NOISY_TEST) 
+    printf("The assert r==1 passed");
+#endif
 
-    int i =0;
-    int j = 0;
-    int n = 0;
-    int testCard;
-    int testPos;
-    int stateCard;
-    int player = floor(Random()*2);
-    int testHand[50];
-//------------------- end prologue, begin tests: 1000 randomized tests  --------------//
+  }
+  else
+  {
+    // test 2: Everything worked and so we should get a 0
+    pre.numActions--;
+    updateCoins(pre.whoseTurn, &pre, coin_bonus);
+#if (DEBUG)
+    assert (r == 0);
+#endif
 
-printf("starting testing for handCard function\n");
+#if (NOISY_TEST) 
+    printf("The assert r == 0 passed");
+#endif
+  }
+  
 
-    //we test handCard by generating a random hand, storing it locally, adding it to the state, and then retrieving the hand from the
-    //state and comparing against the testhand()
+#if (DEBUG)
+  //test3: pre and post should look the same if the function works all the time
+  assert(memcmp(&pre, post, sizeof(struct gameState)) == 0);
+#endif
 
-    for (i;i<1000;i++){
-            for(j;j<50;j++){
-                testCard = floor(Random()*treasure_map);
-                G.hand[player][j] = testCard;
-                testHand[j] = testCard;
-                //if verbose
-                //printf("added %d at handpos %d\n",testCard,j);
-            }
-            testPos = floor(Random()*50);
-            //if verbose
-            stateCard = handCard(testPos,&G);
-            //printf("found card %d at position %d\n",stateCard,testPos);
-            if(stateCard != testHand[testPos]){
-                printf("test hand mismatch\n");
-                errors++;
-            }
+  if (memcmp(&pre, post, sizeof(struct gameState)) != 0) {
+#if (NOISY_TEST) 
+    printf("pre and post don't match");
+#endif
+    result = 1;
+  }
+
+  return result;
+}
+
+int main()
+{
+  //define variables
+  int i, n,p, handPos, c1, c2, c3;
+  int result = 0;
+  
+  //define a gamestate
+  struct gameState G;
+  printf ("Testing playCard with Random Tests\n");
+  SelectStream(2);
+  PutSeed(3);
+
+  //Loop through players, decks and hands to fill the struct
+  // and initiate game play.  
+  for (n = 0; n < 2000; n++)
+  {
+    for (i = 0; i < sizeof(struct gameState); i++)
+    {
+      ((char*)&G)[i] = floor(Random() * 256);
     }
+    handPos = floor(Random() * 15);//number of cards in hand
+    c1 = floor(Random() * 10);//kingdom card index
+    c2 = floor(Random() * 10);
+    c3 = floor(Random() * 10);
+    p = floor(Random() * 2);
+    
+    G.deckCount[p] = floor(Random() * MAX_DECK);
+    G.discardCount[p] = floor(Random() * MAX_DECK);
+    G.handCount[p] = floor(Random() * MAX_HAND);
+    G.phase = floor(Random() * 3);
+    G.numActions = floor(Random() * 16);
+    G.whoseTurn = floor(Random() * 2);
+    //run unit test.
+    result = checkPlayCard(handPos, c1, c2, c3, &G);
+  }
 
+  // print out results
+  if (result == 1) {
+    printf("playCard tests failed.\n");
+  } else {
+    printf("playCard tests passed.\n");
+  }
+  
+  printf ("RANDOM TESTS COMPLETE.\n");
 
-  return 0;
+  exit(0);
 }

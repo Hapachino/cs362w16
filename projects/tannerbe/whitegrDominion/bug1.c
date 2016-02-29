f9e79c570c4b46c55d2124666c2b46e3fc68bcd7
livevil904 <benjamintanner904@gmail.com>
2016-02-29 08:54:07 -0800

    adding teammates dominion folders with tests performed. Forgot to submit last night after getting home, but date modified on files shows that they were last modified before the end of sunday.

---
+++ projects/tannerbe/whitegrDominion/bug1.c
@@ -0,0 +1,11 @@
+1. The unittest1.c tests the scoreFor() function. Within this function the unit test identifies a bug in the scoreFor function if the
+   deck contains a garden card.
+Intentional Bugs are indicated in the prinouts from the unit tests:
+2. The cardtest1 unit test identifies an intentional bug in the smithy card where an addition card is drawn from the player's deck.
+   This unit test also has a bug in checking the smithy discard but did not have enouhg time to track it down.
+3. The cardtest2 test identifies a bug in the Adventure card where only one treasure card is found and added to the players hand.
+4. The cardTest3 identifies an intentional bug in the Council Room card giving the player an extra card.
+5. The cardTest4 identifies an intentional bug in the steward card. Where the card itself is not discarded.
+
+
+  

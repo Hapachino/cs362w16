cd916b292241889faa9b4c4bf526c008a97d7a56
colvinch <colvinch@oregonstate.edu>
2016-02-28 23:41:29 -0800

    Initial Assignment 5 Commit

---
+++ projects/colvinch/dominion/BugFixes.c
@@ -0,0 +1,17 @@
+/*
+
+From bernaca:
+    Issues found:
+
+    Notes:
+
+From gariepyte:
+    Issues found:
+
+    Notes:
+
+From hickej:
+    Issues found:
+
+    Notes:
+*/
---
+++ projects/colvinch/teammate3Dominion/dominion/bug.c
@@ -0,0 +1,28 @@
+Bug report #1   in PlaySmithy()
+
+Bug found in playSmithy funciton through cardTest1.c
+play smithyfunction ran but gave wrong results.
+
+the expected hand count is suppose to be 3 higher than before the function ran
+
+the function gave a handcount of only 1 higher
+
+error could be with the either the calls to drawCard or just the increases in
+handCount.
+
+results in states not matching
+
+
+Bug report #2   in playAdventurer()
+
+Bug found in playAdventurer funciton through cardTest2.c
+play playAdventurer ran but gave wrong results.
+
+had count is usually 1 higher than than it is suppose to be.
+
+
+error could be with the either the calls to drawCard or just the increases in
+handCount.
+
+results in states not matching
+

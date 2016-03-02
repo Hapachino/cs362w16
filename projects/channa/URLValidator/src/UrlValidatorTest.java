6a2fcb6d18066f26c56f87a5188916ae8e0da03e
Nancy Chan <nancychannc@gmail.com>
2016-03-01 19:22:38 -0800

    Added Andrew's test

--- projects/channa/URLValidator/src/UrlValidatorTest.java
+++ projects/channa/URLValidator/src/UrlValidatorTest.java
@@ -39,7 +39,9 @@ public class UrlValidatorTest extends TestCase {
    public void testManualTest()
    {
 	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
-	   //System.out.println(urlVal.isValid("http://www.amazon.com:80/example?test=passed&result=true"));
+	   //System.out.println(urlVal.isValid("http://www.amazon.com:65535/example"));
+	   //System.out.println(urlVal.isValid("http://255.255.255.255"));
+	   //System.out.println(urlVal.isValid("http://256.256.256.256"));
    }
    
    public void testYourFirstPartition()
@@ -152,7 +154,38 @@ public class UrlValidatorTest extends TestCase {
    
    public void testAnyOtherUnitTest()
    {
-	   
+	  int schIdx, hostIdx, portIdx, pathIdx, queryIdx;
+	  String validUrl;
+	  UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	  ResultPair url;
+	  
+	  for(schIdx=0; schIdx<schemes.length; schIdx++)
+	  {
+		  for(hostIdx=0; hostIdx<hosts.length; hostIdx++)
+		  {
+			  for(portIdx=0; portIdx<ports.length; portIdx++)
+			  {
+				  for(pathIdx=0; pathIdx<paths.length; pathIdx++)
+				  {
+					  for(queryIdx=0; queryIdx<queries.length; queryIdx++)
+					  {
+						  url = URLBuilder(schIdx, hostIdx, portIdx, pathIdx, queryIdx);
+						  validUrl = (url.valid) ? "VALID" : "INVALID"; // ternary operators make life easier
+						  
+						  System.out.println("Testing " + validUrl + " URL: " + url.item);
+						  
+						  if(url.valid) {
+							  //assertTrue(urlVal.isValid(url.item));
+						  }
+						  else {
+							  //assertFalse(urlVal.isValid(url.item));
+						  }
+					  }
+				  }
+			  }
+		  }
+	  }
+	  
    }
    /**
     * Create set of tests by taking the testUrlXXX arrays and
@@ -161,5 +194,82 @@ public class UrlValidatorTest extends TestCase {
     * @param testObjects Used to create a url.
     */
    
+   ResultPair[] schemes = {
+		   new ResultPair("http://", true),
+		   new ResultPair("https://", true),
+		   new ResultPair("ftp://", true),
+		   new ResultPair("sftp://", true),
+		   new ResultPair("http:///", false),
+		   new ResultPair("foo://", false),
+		   new ResultPair("bar://", false),
+		   new ResultPair("", true) // this should be true, because the web doesn't necessary penalize you if you don't use a protocol.
+   };
+   
+   ResultPair[] hosts = {
+		   new ResultPair("127.0.0.1", true),
+		   new ResultPair("127.0.1", false),
+		   new ResultPair("www.yahoo.com", true),
+		   new ResultPair("www.amazon.com", true),
+		   new ResultPair("www.bing.com", true),
+		   new ResultPair("yahoo", false),
+		   new ResultPair("amazon", false),
+		   new ResultPair("bing", false),
+		   new ResultPair("baidu", false),
+		   new ResultPair("oregonstate.edu", true),
+		   new ResultPair("", false)
+   };
+   
+	ResultPair[] ports = {
+			   new ResultPair(":80", true),
+			   new ResultPair(":8080", true),
+			   new ResultPair(":65535", true),
+			   new ResultPair(":65536", false),
+			   new ResultPair(":23a", false),
+			   new ResultPair(":-80", false),
+			   new ResultPair("", true)
+	};
+		   
+	ResultPair[] paths = {
+			   new ResultPair("/path", true),
+			   new ResultPair("/path/", true),
+			   new ResultPair("/path2/../path1", true),
+			   new ResultPair("/path2/..path1", false),
+			   new ResultPair("noslashes", false),
+			   new ResultPair("/path/name", true),
+			   new ResultPair("", true)
+	};
+	
+	ResultPair[] queries = {
+			   new ResultPair("?lvalue=rvalue", true),
+			   new ResultPair("?lvalue=rvalue&lvalue2=rvalue2", true),
+			   new ResultPair("?lvalue=rvalue;lvalue2=rvalue2", true),
+			   new ResultPair("?lvalue=rvalue:lvalue2=rvalue2", false),
+			   new ResultPair("?=rvalueonly", false),
+			   new ResultPair("", true)
+	};
+		   
 
+	public ResultPair URLBuilder(int schIdx, int hostIdx, int portIdx, int pathIdx, int queryIdx)
+	{
+		boolean urlValidity;
+		
+		
+		if((schIdx >= schemes.length) || (hostIdx >= hosts.length) || (portIdx >= ports.length) || ( pathIdx >= paths.length)|| (queryIdx >= queries.length)) {
+			return new ResultPair("Out of range", false);
+		}
+		
+		else 
+		{
+			String urlString = schemes[schIdx].item + hosts[hostIdx].item + ports[portIdx].item + paths[pathIdx].item + queries[queryIdx].item;
+	
+			urlValidity = true;
+
+			if(!schemes[schIdx].valid || !hosts[hostIdx].valid || !ports[portIdx].valid || !paths[pathIdx].valid || !queries[pathIdx].valid) urlValidity = false;
+		
+		
+			ResultPair builtURL = new ResultPair(urlString, urlValidity);
+	
+			return builtURL;
+		}
+	}
 }

31fc29a3cddf2158b32aa3ea84eeac449f42e032
Megan Fanning <megantfanning@gmail.com>
2016-03-05 00:32:30 -0800

    added a new test and partition

--- projects/fanninme/URLValidator/URLValidatorRandomTest.java
+++ projects/fanninme/URLValidator/URLValidatorRandomTest.java
@@ -14,9 +14,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-import java.util.UUID;
 import junit.framework.TestCase;
+import java.util.UUID;
+import org.junit.Test;
+import java.util.Random;
+//import static org.junit.Assert.*;
+ 
 
 /**
  * Performs Validation Test for url validations.
@@ -30,17 +33,30 @@ public class URLValidatorRandomTest extends TestCase {
    private boolean printStatus = false;
    private boolean printIndex = false;//print index that indicates current scheme,host,port,path, query test were using.
 
-   //Randomized Testing   
-   public class RandomUrlValidatorTest extends TestCase {
-   private boolean printStatus = false;
-   private boolean printIndex = false;//print index that indicates current scheme,host,port,path, query test were using.
+   //
+   char[] LetterPunct= {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t',
+'u','v','w','x','y','z','-','/','[','&','#','@','!'};
 
    //generate random strings
-   public String RandString()
+   public String RandNumString()
    {
        //create string
        String randuuid = UUID.randomUUID().toString();    
        return randuuid;
+   }
+   //
+   public String RandString(int size)
+   {
+	   Random rand = new Random();
+	   StringBuilder sb = new StringBuilder();
+	   
+	   for(int i=0;i<size;i++){
+		   int value = rand.nextInt(33);//length of LetterPunct  
+		   sb.append(LetterPunct[value]);
+		   //create string
+	   }
+	   String str = sb.toString();
+       return str;
    }
    //address prefix
    String[] UrlPrefix= {
@@ -64,19 +80,22 @@ public class URLValidatorRandomTest extends TestCase {
 			  ".gov",
 			  ".us",
 			  ".html",
-			  "/mail/",
+			  ".com/mail/",
 			  ""};
    
    //IsValid tests
+   @Test
    public void testRandomTest(int i)
    {
 	  UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
-       String randText=RandString();
+       String randText=RandNumString();
        String testURL=UrlPrefix[i]+randText+UrlPostfix[i];
   	   boolean result;
        //URL Validation
   	   System.out.println("Testing valid URL address:"+testURL);
   	   result= urlVal.isValid(testURL);
+  	   assertTrue(result);
+  	   
   	   if (result)
   	   { 
   		   System.out.println("Valid URL:"+testURL+" Pass");
@@ -84,10 +103,36 @@ public class URLValidatorRandomTest extends TestCase {
   	   else {
   		   System.out.println("Valid URL:"+testURL+" Fail");
   	   }
- 	   System.out.println("Test Completed");
+
    }
-   //Do random tests
-   public void testIsValid()
+   
+   
+   @Test
+   public void testRandomlettersTest(int i)
+   {
+	  UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+       String randText=RandString(10);//test 10 character strings
+       String testURL=UrlPrefix[i]+randText+UrlPostfix[i];
+  	   boolean result;
+       //URL Validation
+  	   System.out.println("Testing valid URL address:"+testURL);
+  	   result= urlVal.isValid(testURL);
+  	   assertTrue(result);
+  	   
+  	   if (result)
+  	   { 
+  		   System.out.println("Valid URL:"+testURL+" Pass");
+  	   }
+  	   else {
+  		   System.out.println("Valid URL:"+testURL+" Fail");
+  	   }
+
+   }
+   
+   //Do random tests  
+   //Partition testing
+   //first partition numbers and letters
+   public void testYourFirstPartition()
    {
        int numTests=10;
        //call randomTests
@@ -96,17 +141,14 @@ public class URLValidatorRandomTest extends TestCase {
 		   testRandomTest(i);
 	   }
    }
-
-   }
-
-   //Partition testing
-   public void testYourFirstPartition()
-   {
-	   
-   }
-   
+   //second partition punctuation and letters
    public void testYourSecondPartition(){
-	   
+       int numTests=10;
+       //call randomTests
+	   for(int i = 0;i<numTests;i++)
+	   {
+		   testRandomlettersTest(i);
+	   }
    }
    
    

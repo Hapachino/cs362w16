4dbf2fa83c25d5daa137aaf2eebdc61c7e9b6432
jholkeboer <jholkeboer@users.noreply.github.com>
2016-03-08 12:23:58 -0800

    programmatic tests

--- projects/holkeboj/URLValidator/src/UrlValidatorTest.java
+++ projects/holkeboj/URLValidator/src/UrlValidatorTest.java
@@ -17,7 +17,7 @@
 
 
 import junit.framework.TestCase;
-
+import java.util.Random;
 
 
 
@@ -60,10 +60,141 @@ public class UrlValidatorTest extends TestCase {
    
    public void testIsValid()
    {
-	   for(int i = 0;i<10000;i++)
+	   // Construct sets of ResultPairs that will be randomly chosen from
+	   // These arrays represent the different parts of a URL:
+	   	// Scheme (e.g. http, ftp, etc)
+	   	// Address (e.g. yahoo.com, www.google.com, bbc.co.uk, etc)
+	   	// Port (e.g. no port, :80, :63353, etc)
+	   	// Path (e.g. /path, /path1/path2/file.txt, etc)
+	   	// Query String (e.g. ?value=1)
+	   // The only URL part that must be present is the address. 
+	   // Each other part can have the value of a blank string and boolean true.
+	   
+	   
+	   // Test Schemes
+	   ResultPair[] testSchemes = {
+			   new ResultPair("", true),
+			   new ResultPair("http://", true),
+			   new ResultPair("ftp://", true),
+			   new ResultPair("h3t://", true),
+			   new ResultPair("htpp://", false),
+			   new ResultPair("http", false),
+			   new ResultPair("http:/", false),
+			   new ResultPair("ftp:", false),
+			   new ResultPair("h3tt://", false),
+			   new ResultPair("fft:", false),
+			   new ResultPair(":/", false),
+			   new ResultPair("://", false),
+	   };
+	   
+	   // Test addresses
+	   ResultPair[] testAddresses = {
+			   new ResultPair("yahoo.com", true),
+			   new ResultPair("www.yahoo.com", true),
+			   new ResultPair("www.yahoo.com.au", true),
+			   new ResultPair(".yahoo.com", false),
+			   new ResultPair("192.168.0.110", true),
+			   new ResultPair("300.168.0.110", false),
+			   new ResultPair("google.kjfwoei", false),
+			   new ResultPair("google.", false),
+			   new ResultPair("0.0.0.0", true),
+			   new ResultPair("12345.com", true),
+			   new ResultPair("255.255.255.255", true),
+			   new ResultPair("255.256.255.255", false),
+			   new ResultPair("256.255.255.255", false),
+			   new ResultPair("255.255.256.255", false),
+			   new ResultPair("255.255.255.256", false),
+			   new ResultPair("192.24.2.1.2", false),
+			   new ResultPair("192.168.0.500", false),
+			   new ResultPair("google", false),
+			   new ResultPair(".google", false),
+			   new ResultPair("", false)	
+	   };
+	   
+	   ResultPair[] testPorts = {
+			   new ResultPair("", true),
+			   new ResultPair(":80", true),
+			   new ResultPair(":0", true),
+			   new ResultPair(":-1", false),
+			   new ResultPair(":oisd", false),
+			   new ResultPair(":44isd", false),
+			   new ResultPair(":24ois67", false),
+			   new ResultPair(":66849", false),
+			   new ResultPair(":65635", true)
+	   };
+	   
+	   ResultPair[] testPaths = {
+			   new ResultPair("/testpath", true),
+			   new ResultPair("/i12iro3i", true),
+			   new ResultPair("/testpath/", true),
+			   new ResultPair("/$owie", true),
+			   new ResultPair("/..", false),
+			   new ResultPair("/#", false),
+			   new ResultPair("/#/testpath", false),
+			   new ResultPair("", true),
+			   new ResultPair("/testpath1/testpath2", true),
+			   new ResultPair("testpath1//testpath2", false),
+			   new ResultPair("/testpath1//testpath2", false),
+			   new ResultPair("", true),
+			   new ResultPair("1path/path2", false),
+			   new ResultPair("/1path/path2", true)
+	   };
+	   
+	   ResultPair[] testQueryStrings = {
+			   new ResultPair("?query=this", true),
+			   new ResultPair("?query=this&query2=that", true),
+			   new ResultPair("?query=this&amp;query2=that", true),
+			   new ResultPair("?query=this and that", false),
+			   new ResultPair("", true),
+	   };
+	   
+	   // Each ResultPair has a boolean value.
+	   // We construct a test case by concatenating the strings in each URL part
+	   // And then doing an AND on all the boolean values
+	   int testsFailed = 0;
+	   int numberOfTests = 10000;
+	   Random generator = new Random();
+	   for(int i = 0;i<numberOfTests;i++)
 	   {
+		   // get random scheme
+		   int randomSchemeIndex = generator.nextInt(testSchemes.length);
+		   String scheme = testSchemes[randomSchemeIndex].item;
+		   Boolean schemeValidity = testSchemes[randomSchemeIndex].valid;
 		   
+		   // get random address
+		   int randomAddressIndex = generator.nextInt(testAddresses.length);
+		   String address = testAddresses[randomAddressIndex].item;
+		   Boolean addressValidity = testAddresses[randomAddressIndex].valid;	
+		   
+		   // get random port
+		   int randomPortIndex = generator.nextInt(testPorts.length);
+		   String port = testPorts[randomPortIndex].item;
+		   Boolean portValidity = testPorts[randomPortIndex].valid;
+		   
+		   // get random path
+		   int randomPathIndex = generator.nextInt(testPaths.length);
+		   String path = testPaths[randomPathIndex].item;
+		   Boolean pathValidity = testPaths[randomPathIndex].valid;	
+		   
+		   // get random query string
+		   int randomQueryStringIndex = generator.nextInt(testQueryStrings.length);
+		   String queryString = testQueryStrings[randomQueryStringIndex].item;
+		   Boolean queryStringValidity = testQueryStrings[randomQueryStringIndex].valid;
+		   
+		   // build test case
+		   String testURL = scheme + address + port + path + queryString;
+		   Boolean testURLValidity = schemeValidity && addressValidity && portValidity
+				   && pathValidity && queryStringValidity;
+		   
+		   // test the test case
+		   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_LOCAL_URLS);
+		   boolean result = urlVal.isValid(testURL);
+		   if (result != testURLValidity) {
+			   System.out.format("Failed for URL %s\n", testURL);
+			   testsFailed += 1;
+		   }
 	   }
+	   System.out.format("Failed %d out of %d tests.\n", testsFailed, numberOfTests);
    }
    
    public void testAnyOtherUnitTest()

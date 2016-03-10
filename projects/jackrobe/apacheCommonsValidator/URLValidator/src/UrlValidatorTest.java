3e28cd334a4a57b0d690878e24bd26f965795b4d
Robert Jackson <jackrobe@onid.oregonestate.edu>
2016-03-10 13:25:08 -0700

    Added Random Tessing Added badAuthority test Added Random String generator Added a bunch of todo :D

--- projects/jackrobe/apacheCommonsValidator/URLValidator/src/UrlValidatorTest.java
+++ projects/jackrobe/apacheCommonsValidator/URLValidator/src/UrlValidatorTest.java
@@ -23,6 +23,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 
 /**
@@ -48,6 +50,92 @@ class helperFunctions {
         return lines.toArray(new String[lines.size()]);
     }
 
+    //Makes part of the URL invalid
+    //@ param string array of urls that ARE valid
+    //@param part (which part you want to make invalid: SEE switch Statement)
+    //Returns  a List<String> of now BAD urls
+
+    //TODO needs fixin...still not making good-bad URLS'-its set to print during test run
+    public List<String> makeInvalidPart(String[] urls, int part){
+
+        //defines
+        URL aURL = null;
+        String auth, path, query, protocol, host;
+        List<String> badUrls = new ArrayList<String>();
+        RandomString rnd, rnd1, rnd2, rnd3;
+
+        try {
+
+            for (String url: urls) {
+
+                //Parse the URL
+                aURL = new URL(url);
+
+                auth = aURL.getAuthority();
+                path = aURL.getPath();
+                query = aURL.getQuery();
+                host = aURL.getHost();
+                protocol = aURL.getProtocol();
+
+
+                //Select which part to change
+                //Todo I'm missing one section of the URL the main part HOST, and... ports?
+                switch (part){
+                    case 1: //BAD AUTHORITY
+
+                        //some length 3 some length 2 authorities
+                        //todo needs some tweaking to make more halfway plausiable authorities i.e. not all ASCII
+                        int half = ThreadLocalRandom.current().nextInt(0, 2);//http://stackoverflow.com/questions/363681/generating-random-integers-in-a-specific-ran
+
+                        if (half == 0 ) {
+                             rnd = new RandomString(3);
+
+                        }else{
+                             rnd = new RandomString(2);
+                        }
+
+                        auth = "." + rnd.nextString();
+                        badUrls.add(protocol +"://" + host + auth);
+
+                        break;
+
+                    case 2: //BAD PATH
+
+                        rnd1 = new RandomString(ThreadLocalRandom.current().nextInt(1, 50));
+
+                        path = "/" + rnd1.nextString() ;
+                        badUrls.add(protocol +"://" + host + path);
+                        break;
+
+                    case 3: //BAD query
+
+                        //TODO finish query
+                        query = aURL.getQuery();
+
+                        break;
+
+                    case 4: //BAD Protocol
+
+                        rnd2 =  new RandomString(ThreadLocalRandom.current().nextInt(2, 5));
+                        protocol = rnd2.nextString() + "://";
+
+                        badUrls.add(protocol + host);
+                    case 5: //BAD ...
+
+                        break;
+                }
+
+
+
+            }
+
+        }catch(IOException ex){
+            System.out.println (ex.toString());
+            System.out.println("Could not find file " );
+        }
+
+        return badUrls;
+    }
 
 }
 
@@ -61,9 +149,9 @@ public class UrlValidatorTest extends TestCase {
       super(testName);
    }
 
-   
-   
-   public void testKnownValid()
+
+    //Tests Known Valid URLS, using a text file
+    public void testKnownValid()
    {
 	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
 
@@ -72,23 +160,24 @@ public class UrlValidatorTest extends TestCase {
        helperFunctions h = new helperFunctions();
        int failRate =0;
        List<String> failedUrls = new ArrayList<String>();
-       boolean testWhole, testAuth, testPath, testPort, testQu, testScheme ;
+       boolean testWhole, testAuth, testPath, testHost, testQu, testProtocol ;
+
+
        try {
            String[] urls = h.readLines(fileName);
 
            //pass them to isValid
            for (String url: urls) {
                URL aURL = new URL(url);
+               //TODO make it test the host too!
+               //todo make sure I'm hitting all the parts
 
                testAuth = urlVal.isValidAuthority(aURL.getAuthority());
                testPath = urlVal.isValidPath(aURL.getPath());
                testQu = urlVal.isValidQuery(aURL.getQuery());
-               testScheme = urlVal.isValidScheme(aURL.getProtocol());
+               testProtocol = urlVal.isValidScheme(aURL.getProtocol());
                testWhole = urlVal.isValid(url);
 
-               //todo I know this next section needs to be refactored but I have to run and want the thought/idea to be there
-               //when I come back Also it's not giving useful information at all, I thought I could add more than one assert to thetest
-               // but I guess I'm mistaken
 
                if (!testAuth) {
 
@@ -96,21 +185,23 @@ public class UrlValidatorTest extends TestCase {
                    System.out.println("Authority failed ");
                }
 
-               if (!testPath) {
+               else if (!testPath) {
 
                    //assertEquals(true,testPath);
                    System.out.println("Test Path failed");
                }
 
-               if (!testQu) {
+               else if (!testQu) {
 
                    System.out.println("Query failed");
                }
-               if (!testScheme) {
+
+               else if (!testProtocol) {
 
 
                    System.out.println("Scheme failed");
                }
+
 
                if (!testWhole) {
 
@@ -119,77 +210,151 @@ public class UrlValidatorTest extends TestCase {
                    System.out.println("URL: " + url);
                }
 
-               if (failRate > 0) {
-
-                   System.out.println("Failed Testing " + failRate + " test(s) of " + urls.length);
-                   System.out.println("The following Urls Failed " + failedUrls);
-
-               }
            }
 
-                   assertEquals(0, failRate);
+            if(failRate > 0) {
+                System.out.println("Failed Testing " + failRate + " test(s) of " + urls.length);
+                System.out.println("The following Urls Failed " + failedUrls);
+            }else{
+                System.out.println("Passed"+ urls.length+ " test(s)");
+            }
+
+           assertEquals(0, failRate);  // Makes sure
+
        }catch(IOException ex){
            System.out.println (ex.toString());
            System.out.println("Could not find file " + fileName);
        }
 
 
-
-
-
-       //System.out.println(urlVal.isValid("http://www.amazon.com"));
-
-
-       //print out what didn't pass
-
-
-
    }
 
-    public void knownInvalidTest()
-    {
-        UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
 
-        //read in Valid urls
-
-        //pass them to isValid
-        System.out.println(urlVal.isValid("http://www.amazon.com"));
-
-        ////print out what did pass
-
-    }
-   
-   
-   public void testAuthority()
-   {
-	   
-   }
-   
    public void testPort(){
-	   
+       //todo add code similar to testBadAuthority()
    }
-   
-   
+
+
    public void testPath()
    {
-
+       //todo add code similar to testBadAuthority()
    }
-
+    public void testHost()
+    {
+        //todo add code similar to testBadAuthority()
+    }
     public void testQuery()
     {
+        //todo add code similar to testBadAuthority()
+    }
+    public void testProtocol()
+    {
+        //todo add code similar to testBadAuthority()
+    }
+
+    public void testPRINTBAD()
+    {
+        //define some stuff
+        UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+        helperFunctions h = new helperFunctions();
+        List<String> failedUrls = new ArrayList<String>();
+        List<String> badAutho = new ArrayList<String>();
+        List<String> badPath = new ArrayList<String>();
+        List<String> badProto = new ArrayList<String>();
+
+
+        String fileName = "src\\UrlsToVerify.txt";
+        String[] urls = null;
+
+        boolean testWhole, testAuth, testPath, testPort, testQu, testScheme ;
+
+        URL aURL = null;
+
+        //read in Valid urls
+        try{
+            urls = h.readLines(fileName);
+
+        }catch(IOException ex){
+            System.out.println (ex.toString());
+            System.out.println("Could not find file " + fileName);
+        };
+
+
+        badAutho = h.makeInvalidPart(urls, 1);
+        badPath = h.makeInvalidPart(urls, 2);
+        badProto = h.makeInvalidPart(urls, 4);
+        System.out.println("BAD AUTHORITY URLS: " + badAutho);
+        System.out.println("BAD Path URLS: " + badPath);
+        System.out.println("BAD Proto URLS: " + badProto);
+
 
     }
 
-    public void testAnyOtherUnitTest()
-   {
-	   
-   }
-   /**
-    * Create set of tests by taking the testUrlXXX arrays and
-    * running through all possible permutations of their combinations.
-    *
-    * @param testObjects Used to create a url.
-    */
+    public void testBadAuthority() {
+
+        //define some stuff
+        UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+        helperFunctions h = new helperFunctions();
+        List<String> failedUrls = new ArrayList<String>();
+        List<String> badUrls = new ArrayList<String>();
+
+        String fileName = "src\\UrlsToVerify.txt";
+        String[] urls = null;
+
+        int failRate =0;
+        boolean testWhole, testAuth, testPath, testPort, testQu, testScheme ;
+
+        URL aURL = null;
+
+
+        //read in Valid urls
+        try{
+            urls = h.readLines(fileName);
+        }catch(IOException ex){
+                System.out.println (ex.toString());
+                System.out.println("Could not find file " + fileName);
+        };
+
+        //TODO I'm only changing one part; we'll need to do this for all parts
+        //TODO refactor so that it's portable to checking different parts
+        //TODO add this to separate tests
+        badUrls = h.makeInvalidPart(urls, 1);
+
+        //Mostly the same as the KnownValid except looking for passing b/c it should be failing in these cases
+        //todo possibly refactor if we make more
+        //pass them to isValid
+
+        for (String url: badUrls) {
+            try {
+                aURL = new URL(url);
+
+            }catch(IOException ex) {
+
+                System.out.println (ex.toString());
+                System.out.println("URL didn't work " + fileName);
+            }
+
+            testWhole = urlVal.isValid(url);
+
+            if (testWhole) {
+
+                failRate++;
+                failedUrls.add(url);
+                System.out.println("URL: " + url);
+            }
+        }
+
+        if (failRate > 0) {
+
+            System.out.println("Failed Testing " + failRate + " test(s) of " + urls.length);
+            System.out.println("The following Urls Passed But MAYBE should not have " + failedUrls);
+
+        }else{
+            System.out.println("Passed"+ urls.length+ " test(s)");
+        }
+        assertEquals(0, failRate);
+
+    }
    
 
 }

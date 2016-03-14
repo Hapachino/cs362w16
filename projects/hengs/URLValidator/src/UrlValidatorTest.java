72257b4507704aa5e8166e29e197c8852490418b
hengs <hengs@onid.oregonstate.edu>
2016-03-14 15:50:04 -0700

    Added Manual tests

--- projects/hengs/URLValidator/src/UrlValidatorTest.java
+++ projects/hengs/URLValidator/src/UrlValidatorTest.java
@@ -46,6 +46,38 @@ public class UrlValidatorTest extends TestCase {
 	   
    }
    
+   public class manualTest {
+	public static void main (String[] args) {
+		int i;
+		String testGoodURLS[] = {"www.google.com", "http://google.com/test", "https://amazon.co.uk", "www.oregonstate.edu"};
+		String testBadURLS[] = {"httq://www.google.com", "http://google.com/$test", "www.$$$$.com", "www.amazon.com:-1"};
+		UrlValidator validator = new UrlValidator();
+
+		for (i = 0; i < 4; i++) {
+			System.out.println("Input: " + testGoodURLS[i]);
+			System.out.print("Expected Result: Test passed");
+
+			if(validator.isValid(testGoodURLS[i])) {
+				System.out.println("Actual Result: Test passed");
+			}
+			else
+				System.out.println("Actual Result: Test failed");
+		}
+
+		for (i = 0; i < 4; i++) {
+			System.out.println("Input: " + testBadURLS[i]);
+			System.out.print("Expected Result: Test failed");
+
+			if(validator.isValid(testBadURLS[i])) {
+				System.out.println("Actual Result: Test passed");
+			}
+			else
+				System.out.println("Actual Result: Test failed");
+		}
+
+	}
+}
+   
    
    public void testYourFirstPartition()
    {
